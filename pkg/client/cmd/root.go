2afaebaabf60a885bc02d1da53d842abb7497917
nferraro <ni.ferraro@gmail.com>
2018-09-08 01:08:40 +0200

    Add get and detection of current namespace

--- pkg/client/cmd/root.go
+++ pkg/client/cmd/root.go
@@ -18,21 +18,43 @@ limitations under the License.
 package cmd
 
 import (
+	"os"
 	"github.com/spf13/cobra"
+	"github.com/apache/camel-k/pkg/client/cmd/get"
+	"github.com/apache/camel-k/pkg/util/kubernetes"
+	"github.com/pkg/errors"
 )
 
+type KamelCmdOptions struct {
+	KubeConfig	string
+	Namespace	string
+}
+
+
 func NewKamelCommand() (*cobra.Command, error) {
+	options := KamelCmdOptions{}
 	var cmd = cobra.Command{
 		Use:   "kamel",
 		Short: "Kamel is a awesome client tool for running Apache Camel integrations natively on Kubernetes",
 		Long:  "Apache Camel K (a.k.a. Kamel) is a lightweight integration framework\nbuilt from Apache Camel that runs natively on Kubernetes and is\nspecifically designed for serverless and microservice architectures.",
 	}
 
-	var kubeconfig string
-	cmd.PersistentFlags().StringVar(&kubeconfig, "config", "", "Path to the config file to use for CLI requests")
+	cmd.PersistentFlags().StringVar(&options.KubeConfig, "config", "", "Path to the config file to use for CLI requests")
+	cmd.PersistentFlags().StringVarP(&options.Namespace, "namespace", "n", "", "Namespace to use for all operations")
+
+	// Parse the flags before setting the defaults
+	cmd.ParseFlags(os.Args)
+
+	if options.Namespace == "" {
+		current, err := kubernetes.GetClientCurrentNamespace(options.KubeConfig)
+		if err != nil {
+			return nil, errors.Wrap(err, "cannot get current namespace")
+		}
+		cmd.Flag("namespace").Value.Set(current)
+	}
 
 	// Initialize the Kubernetes client to allow using the operator-sdk
-	err := initKubeClient(&cmd)
+	err := kubernetes.InitKubeClient(options.KubeConfig)
 	if err != nil {
 		return nil, err
 	}
@@ -40,6 +62,7 @@ func NewKamelCommand() (*cobra.Command, error) {
 	cmd.AddCommand(NewCmdCompletion())
 	cmd.AddCommand(NewCmdVersion())
 	cmd.AddCommand(NewCmdRun())
+	cmd.AddCommand(get.NewCmdGet())
 
 	return &cmd, nil
 }
