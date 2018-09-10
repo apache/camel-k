a4b7633019021df811f4e8a7a5ea7493a1e23f85
nferraro <ni.ferraro@gmail.com>
2018-09-10 11:19:55 +0200

    Add kamel install (docker image still incomplete)

---
+++ pkg/client/cmd/install.go
@@ -0,0 +1,58 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	installutils "github.com/apache/camel-k/pkg/install"
+	"fmt"
+	"k8s.io/apimachinery/pkg/api/errors"
+)
+
+type InstallCmdOptions struct {
+}
+
+func NewCmdInstall() *cobra.Command {
+	options := InstallCmdOptions{}
+	cmd := cobra.Command{
+		Use:   "install",
+		Short: "Install Camel K on a Kubernetes cluster",
+		Long:  `Installs Camel K on a Kubernetes or Openshift cluster.`,
+		RunE:  options.install,
+	}
+	return &cmd
+}
+
+func (o *InstallCmdOptions) install(cmd *cobra.Command, args []string) error {
+	err := installutils.SetupClusterwideResources()
+	if err != nil && errors.IsForbidden(err) {
+		// TODO explain that this is a one time operation and add a flag to do cluster-level operations only when logged as admin
+		fmt.Println("Current user is not authorized to create cluster-wide objects like custom resource definitions or cluster roles: ", err)
+		fmt.Println("Please login as cluster-admin to continue the installation.")
+		return nil // TODO better error handling: if here we return err the help page is shown
+	}
+
+	namespace := cmd.Flag("namespace").Value.String()
+
+	err = installutils.InstallOperator(namespace)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Camel K installed in namespace", namespace)
+	return nil
+}
---
+++ pkg/install/cluster.go
@@ -0,0 +1,124 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package install
+
+import (
+	"github.com/operator-framework/operator-sdk/pkg/k8sclient"
+	"github.com/apache/camel-k/deploy"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"github.com/apache/camel-k/pkg/util/kubernetes/customclient"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"github.com/apache/camel-k/pkg/util/kubernetes"
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	"k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func SetupClusterwideResources() error {
+	// Installing CRD
+	crdInstalled, err := isCRDInstalled()
+	if err != nil {
+		return err
+	}
+	if !crdInstalled {
+		if err := installCRD(); err != nil {
+			return err
+		}
+	}
+
+	// Installing ClusterRole
+	clusterRoleInstalled, err := isClusterRoleInstalled()
+	if err != nil {
+		return err
+	}
+	if !clusterRoleInstalled {
+		err := installClusterRole()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func isCRDInstalled() (bool, error) {
+	lst, err := k8sclient.GetKubeClient().Discovery().ServerResourcesForGroupVersion("camel.apache.org/v1alpha1")
+	if err != nil && errors.IsNotFound(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	for _, res := range lst.APIResources {
+		if res.Kind == "Integration" {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func installCRD() error {
+	crd := []byte(deploy.Resources["crd.yaml"])
+	crdJson, err := yaml.ToJSON(crd)
+	if err != nil {
+		return err
+	}
+	restClient, err := customclient.GetClientFor("apiextensions.k8s.io", "v1beta1")
+	if err != nil {
+		return err
+	}
+	// Post using dynamic client
+	result := restClient.
+		Post().
+		Body(crdJson).
+		Resource("customresourcedefinitions").
+		Do()
+	// Check result
+	if result.Error() != nil && !errors.IsAlreadyExists(result.Error()) {
+		return result.Error()
+	}
+
+	return nil
+}
+
+func isClusterRoleInstalled() (bool, error) {
+	clusterRole := v1.ClusterRole{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRole",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "camel-k:edit",
+		},
+	}
+	err := sdk.Get(&clusterRole)
+	if err != nil && errors.IsNotFound(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func installClusterRole() error {
+	obj, err := kubernetes.LoadResourceFromYaml(deploy.Resources["user-cluster-role.yaml"])
+	if err != nil {
+		return err
+	}
+
+	return sdk.Create(obj)
+}
