92e956b043c1219169c445b601be17cd935411ce
lburgazzoli <lburgazzoli@gmail.com>
2018-09-11 08:00:28 +0200

    remove import alias and fix naming

--- pkg/install/operator.go
+++ pkg/install/operator.go
@@ -25,7 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func InstallOperator(namespace string) error {
+func Operator(namespace string) error {
 	return installResources(namespace,
 		"operator-service-account.yaml",
 		"operator-role.yaml",
