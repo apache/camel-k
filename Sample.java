e4117c85d07926c2a37a092b4ae5e98bb90789cf
Andrea Tarocchi <atarocch@redhat.com>
2018-09-11 15:45:24 +0200

    Changed log message in Sample.java in order to really rocks!

--- Sample.java
+++ Sample.java
@@ -7,8 +7,8 @@ public class Routes extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 	  from("timer:tick")
-		.setBody(constant("Hello! Camel K rocks!!!"))
-		.to("log:info");
+    .setBody(constant("-\n             r\n             o\n             c\nHello! Camel K\n             s\n             !\n"))
+		.to("log:info?skipBodyLineSeparator=false");
   }
 
 }
