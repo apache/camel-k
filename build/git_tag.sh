712d9d888442c8b7dd397da962f7c43024d47f15
nferraro <ni.ferraro@gmail.com>
2018-09-13 19:13:04 +0200

    Update release with git tag step

---
+++ build/git_tag.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+location=$(dirname $0)
+version=$($location/get_version.sh)
+
+git branch -D staging-$version || true
+git checkout -b staging-$version
+git add * || true
+git commit -m "Release $version"
+git tag --force $version staging-$version
+git push --force upstream $version
+
+echo "Tag $version pushed upstream"
