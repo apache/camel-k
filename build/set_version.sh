5efc8a94cb7a5af3565c9bbfddd64e2fd6cf2b3b
nferraro <ni.ferraro@gmail.com>
2018-09-14 10:01:07 +0200

    Add command to increment version

---
+++ build/set_version.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+location=$(dirname $0)
+new_version=$1
+
+global_version_file=$location/../version/version.go
+version=$($location/get_version.sh)
+
+# Set the new global version
+sed -i "s/$version/$new_version/g" $global_version_file
+find $location/../deploy -type f -exec sed -i "s/$version/$new_version/g" {} \;
+
+# Updating the Java modules
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$new_version -f $location/../runtime
+
+echo "Camel K version set to: $new_version"
+
