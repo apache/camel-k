bb9a980a9f08885854deca173abf7c20e3575cde
Andrea Cosentino <ancosen@gmail.com>
2018-09-17 15:50:54 +0200

    Added the possibility to specify a namespace

--- build/install_minishift.sh
+++ build/install_minishift.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-oc login -u system:admin
+if [ "$project" = "" ]; then
+  project="myproject"
+fi
+echo $project
+if [ "$project" != "myproject" ]; then
+  oc new-project $project
+  oc project $project
+fi
+oc login -u system:admin 
 make
 eval $(minishift docker-env)
 make images
 ./kamel install --cluster-setup
 oc delete pod -l name=camel-k-operator
-oc login -u developer
+oc login -u developer 
+if [ "$project" != "myproject" ]; then
+  oc project $project
+fi
 ./kamel install
 
