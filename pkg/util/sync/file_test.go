3c0e60042a299fc50d74bbb2f7bb2224fc1d9967
nferraro <ni.ferraro@gmail.com>
2018-09-18 15:58:34 +0200

    Add sync and dev mode

---
+++ pkg/util/sync/file_test.go
@@ -0,0 +1,65 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sync
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestFile(t *testing.T) {
+	tempdir := os.TempDir()
+	fileName := path.Join(tempdir, "camel-k-test-"+strconv.FormatUint(rand.Uint64(), 10))
+	_, err := os.Create(fileName)
+	assert.Nil(t, err)
+	defer os.Remove(fileName)
+
+	ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(100*time.Second))
+	changes, err := File(ctx, fileName)
+	assert.Nil(t, err)
+
+	time.Sleep(100 * time.Millisecond)
+	expectedNumChanges := 3
+	for i := 0; i < expectedNumChanges; i++ {
+		ioutil.WriteFile(fileName, []byte("data-"+strconv.Itoa(i)), 0777)
+		time.Sleep(350 * time.Millisecond)
+	}
+
+	numChanges := 0
+watch:
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-changes:
+			numChanges++
+			if (numChanges == expectedNumChanges) {
+				break watch
+			}
+		}
+	}
+
+	assert.Equal(t, expectedNumChanges, numChanges)
+}
