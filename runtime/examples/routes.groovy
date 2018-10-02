9b9a1df7284f71afdf132dc5f037cd991bdb9e2d
lburgazzoli <lburgazzoli@gmail.com>
2018-10-02 18:10:28 +0200

    runtime: improve groovy dsl

--- runtime/examples/routes.groovy
+++ runtime/examples/routes.groovy
@@ -1,16 +1,38 @@
+import java.util.concurrent.ThreadLocalRandom
+
 //
 // To run this integrations use:
 //
-//     kamel run -d camel:groovy runtime/examples/routes.groovy
+//     kamel run --runtime groovy runtime/examples/routes.groovy
 //
 
-rnd = new Random()
+context {
+
+    //
+    // configure components
+    //
+    components {
+        'log' {
+            formatter {
+                'body: ' + it.in.body + ', random-value: ' + it.in.headers['RandomValue']
+            }
+        }
+    }
+
+    //
+    // configure registry
+    //
+    registry {
+        bind 'myProcessor', processor {
+            it.in.headers['RandomValue'] = ThreadLocalRandom.current().nextInt()
+        }
+    }
+}
+
 
 from('timer:groovy?period=1s')
     .routeId('groovy')
     .setBody()
         .constant('Hello Camel K!')
-    .process {
-        it.in.headers['RandomValue'] = rnd.nextInt()
-    }
-    .to('log:info?showHeaders=true')
+    .process('myProcessor')
+    .to('log:info')
