cf5676a0b834ba33c51b74817794ee8c98e2b100
nferraro <ni.ferraro@gmail.com>
2019-01-11 11:41:20 +0100

    Fix #312: refactor platform image lookup into its own package

--- pkg/controller/integration/util.go
+++ pkg/controller/integration/util.go
@@ -19,13 +19,21 @@ package integration
 
 import (
 	"context"
-
-	"github.com/apache/camel-k/pkg/util"
-	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"fmt"
 
 	"github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
+	"github.com/apache/camel-k/pkg/client"
+	"github.com/apache/camel-k/pkg/platform/images"
+	"github.com/apache/camel-k/pkg/util"
 	"github.com/pkg/errors"
+	"github.com/rs/xid"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+var allowedLookupLabels = map[string]bool{
+	v1alpha1.IntegrationContextTypePlatform: true,
+	v1alpha1.IntegrationContextTypeExternal: true,
+}
 
 // LookupContextForIntegration --
 func LookupContextForIntegration(ctx context.Context, c k8sclient.Reader, integration *v1alpha1.Integration) (*v1alpha1.IntegrationContext, error) {
@@ -50,7 +58,7 @@ func LookupContextForIntegration(ctx context.Context, c k8sclient.Reader, integr
 
 	for _, ctx := range ctxList.Items {
 		ctx := ctx // pin
-		if ctx.Labels["camel.apache.org/context.type"] == v1alpha1.IntegrationContextTypePlatform {
+		if allowed, ok := allowedLookupLabels[ctx.Labels["camel.apache.org/context.type"]]; ok && allowed {
 			ideps := len(integration.Status.Dependencies)
 			cdeps := len(ctx.Spec.Dependencies)
 
@@ -65,4 +73,32 @@ func LookupContextForIntegration(ctx context.Context, c k8sclient.Reader, integr
 	}
 
 	return nil, nil
+}
+
+// ImportPredefinedContextIfPresent tries to create an external context from a predefined image
+func ImportPredefinedContextIfPresent(ctx context.Context, c client.Client, integration *v1alpha1.Integration) (*v1alpha1.IntegrationContext, error) {
+	image := images.LookupPredefinedImage(integration.Status.Dependencies)
+	if image == "" {
+		return nil, nil
+	}
+
+	externalCtxName := fmt.Sprintf("ctx-base-%s", xid.New())
+	externalCtx := v1alpha1.NewIntegrationContext(integration.Namespace, externalCtxName)
+
+	externalCtx.Labels = map[string]string{
+		"camel.apache.org/context.type":               v1alpha1.IntegrationContextTypeExternal,
+		"camel.apache.org/context.created.by.kind":    v1alpha1.IntegrationKind,
+		"camel.apache.org/context.created.by.name":    integration.Name,
+		"camel.apache.org/context.created.by.version": integration.ResourceVersion,
+	}
+
+	externalCtx.Spec = v1alpha1.IntegrationContextSpec{
+		Dependencies: integration.Status.Dependencies,
+		Image:        image,
+	}
+
+	if err := c.Create(ctx, &externalCtx); err != nil {
+		return nil, err
+	}
+	return &externalCtx, nil
 }
---
+++ pkg/platform/images/images_test.go
@@ -0,0 +1,68 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package images
+
+import (
+	"github.com/apache/camel-k/version"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestImageLookup(t *testing.T) {
+	cases := []struct {
+		dependencies []string
+		image        string
+	}{
+		{
+			dependencies: []string{"camel:telegram"},
+		},
+		{
+			dependencies: []string{"camel:telegram", "camel:core"},
+		},
+		{
+			dependencies: []string{"camel:telegram", "camel:core", "camel-k:knative"},
+			image:        BaseRepository + "/" + ImagePrefix + "telegram:" + version.Version,
+		},
+		{
+			dependencies: []string{"camel:core", "camel-k:knative"},
+			image:        BaseRepository + "/" + ImagePrefix + "core:" + version.Version,
+		},
+		{
+			dependencies: []string{"camel:dropbox", "camel:core", "camel-k:knative", "runtime:jvm"},
+			image:        BaseRepository + "/" + ImagePrefix + "dropbox:" + version.Version,
+		},
+		{
+			dependencies: []string{"camel:dropbox", "camel:core", "camel-k:knative", "runtime:jvm", "runtime:yaml"},
+			image:        BaseRepository + "/" + ImagePrefix + "dropbox:" + version.Version,
+		},
+		{
+			dependencies: []string{"camel:dropbox", "camel:core", "runtime:jvm", "runtime:yaml"},
+		},
+		{
+			dependencies: []string{"camel:dropbox", "camel:core", "camel-k:knative", "runtime:jvm", "runtime:groovy"},
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run("case-"+strconv.Itoa(i), func(t *testing.T) {
+			assert.Equal(t, tc.image, LookupPredefinedImage(tc.dependencies))
+		})
+	}
+
+}
