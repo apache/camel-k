0810f1803937fe04606cc930727adf6b7407456f
Roland Huß <roland@ro14nd.de>
2019-01-21 11:31:48 +0100

    chore: regenerated resources.go
    ... and ensure that the resources are picked up in alphabetical order

--- deploy/resources.go
+++ deploy/resources.go
@@ -2798,6 +2798,29 @@ artifacts:
     version: 2.23.1
 
 `
+	Resources["cr-example.yaml"] =
+		`
+apiVersion: camel.apache.org/v1alpha1
+kind: Integration
+metadata:
+  name: example
+spec:
+  source:
+    content: |-
+      // This is Camel K Groovy example route
+
+      rnd = new Random()
+
+      from('timer:groovy?period=1s')
+          .routeId('groovy')
+          .setBody()
+              .constant('Hello Camel K!')
+          .process {
+              it.in.headers['RandomValue'] = rnd.nextInt()
+          }
+          .to('log:info?showHeaders=true')
+    name: routes.groovy
+`
 	Resources["crd-integration-context.yaml"] =
 		`
 apiVersion: apiextensions.k8s.io/v1beta1
@@ -2887,29 +2910,6 @@ spec:
       description: The IntegrationContext to use
       JSONPath: .status.context
 
-`
-	Resources["cr-example.yaml"] =
-		`
-apiVersion: camel.apache.org/v1alpha1
-kind: Integration
-metadata:
-  name: example
-spec:
-  source:
-    content: |-
-      // This is Camel K Groovy example route
-
-      rnd = new Random()
-
-      from('timer:groovy?period=1s')
-          .routeId('groovy')
-          .setBody()
-              .constant('Hello Camel K!')
-          .process {
-              it.in.headers['RandomValue'] = rnd.nextInt()
-          }
-          .to('log:info?showHeaders=true')
-    name: routes.groovy
 `
 	Resources["operator-deployment-kubernetes.yaml"] =
 		`
