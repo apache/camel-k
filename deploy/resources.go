93fbc4fda708a94ccbc25494f33b48e4448ae5ed
nferraro <ni.ferraro@gmail.com>
2019-01-22 01:41:10 +0100

    Release 0.2.0

--- deploy/resources.go
+++ deploy/resources.go
@@ -1545,7 +1545,7 @@ artifacts:
     - http: true
       id: knative
       passive: false
-    version: 0.2.0-SNAPSHOT
+    version: 0.2.0
   camel-krati:
     artifactId: camel-krati
     dataformats: []
@@ -2936,7 +2936,7 @@ spec:
       serviceAccountName: camel-k-operator
       containers:
         - name: camel-k-operator
-          image: docker.io/apache/camel-k:0.2.0-SNAPSHOT
+          image: docker.io/apache/camel-k:0.2.0
           ports:
           - containerPort: 60000
             name: metrics
@@ -2995,7 +2995,7 @@ spec:
       serviceAccountName: camel-k-operator
       containers:
         - name: camel-k-operator
-          image: docker.io/apache/camel-k:0.2.0-SNAPSHOT
+          image: docker.io/apache/camel-k:0.2.0
           ports:
           - containerPort: 60000
             name: metrics
--- version/version.go
+++ version/version.go
@@ -20,5 +20,5 @@ package version
 
 var (
 	// Version is the global Camel K Version
-	Version = "0.2.0-SNAPSHOT"
+	Version = "0.2.0"
 )
