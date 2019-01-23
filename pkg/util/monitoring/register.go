eb18b60d53bfdcc0ba3d327ba2e08cd4f25bb8a9
Antonin Stefanutti <antonin@stefanutti.fr>
2019-01-23 15:10:07 +0100

    Prometheus trait initial implementation

---
+++ pkg/util/monitoring/register.go
@@ -0,0 +1,47 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package monitoring
+
+import (
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type registerFunction func(*runtime.Scheme) error
+
+// AddToScheme adds monitoring types to the scheme
+func AddToScheme(scheme *runtime.Scheme) error {
+	var err error
+
+	err = doAdd(monitoringv1.AddToScheme, scheme, err)
+
+	return err
+}
+
+func doAdd(addToScheme registerFunction, scheme *runtime.Scheme, err error) error {
+	callErr := addToScheme(scheme)
+	if callErr != nil {
+		logrus.Error("Error while registering monitoring types", callErr)
+	}
+
+	if err == nil {
+		return callErr
+	}
+	return err
+}
