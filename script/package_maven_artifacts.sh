dc0fc6c010a015d8ae1c8425c81054dad5c79b54
Antonin Stefanutti <antonin@stefanutti.fr>
2019-02-13 10:21:54 +0100

    Set Maven batch mode for CI Maven build

--- script/package_maven_artifacts.sh
+++ script/package_maven_artifacts.sh
@@ -2,4 +2,4 @@
 
 location=$(dirname $0)
 cd $location/../
-./mvnw clean install -DskipTests -f runtime/pom.xml -s build/maven/settings.xml
+./mvnw clean install --batch-mode -DskipTests -f runtime/pom.xml -s build/maven/settings.xml
