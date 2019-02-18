751294201e3d79f985655e87e8c95dc34054f6fb
lburgazzoli <lburgazzoli@gmail.com>
2019-02-18 14:09:28 +0100

    chore(catalog): improve camel catalog generation

--- runtime/camel-k-maven-plugin/src/main/java/org/apache/camel/k/tooling/maven/GenerateCatalogMojo.java
+++ runtime/camel-k-maven-plugin/src/main/java/org/apache/camel/k/tooling/maven/GenerateCatalogMojo.java
@@ -23,14 +23,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.ServiceLoader;
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.StreamSupport;
 
-import org.apache.camel.catalog.CamelCatalog;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import org.apache.camel.catalog.DefaultCamelCatalog;
 import org.apache.camel.k.tooling.maven.model.CamelArtifact;
 import org.apache.camel.k.tooling.maven.model.CatalogComponentDefinition;
@@ -38,6 +41,9 @@ import org.apache.camel.k.tooling.maven.model.CatalogDataFormatDefinition;
 import org.apache.camel.k.tooling.maven.model.CatalogLanguageDefinition;
 import org.apache.camel.k.tooling.maven.model.CatalogProcessor;
 import org.apache.camel.k.tooling.maven.model.CatalogSupport;
+import org.apache.camel.k.tooling.maven.model.crd.CamelCatalog;
+import org.apache.camel.k.tooling.maven.model.crd.CamelCatalogSpec;
+import org.apache.camel.k.tooling.maven.model.k8s.ObjectMeta;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -47,14 +53,6 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.introspector.Property;
-import org.yaml.snakeyaml.nodes.CollectionNode;
-import org.yaml.snakeyaml.nodes.Node;
-import org.yaml.snakeyaml.nodes.NodeTuple;
-import org.yaml.snakeyaml.nodes.Tag;
-import org.yaml.snakeyaml.representer.Representer;
 
 @Mojo(
     name = "generate-catalog",
@@ -93,8 +91,8 @@ public class GenerateCatalogMojo extends AbstractMojo {
             throw new MojoExecutionException("Exception while generating camel catalog", e);
         }
 
-        final Map<String, CamelArtifact> artifacts = new TreeMap<>();
-        final CamelCatalog catalog = new DefaultCamelCatalog();
+        final SortedMap<String, CamelArtifact> artifacts = new TreeMap<>();
+        final org.apache.camel.catalog.CamelCatalog catalog = new DefaultCamelCatalog();
 
         try {
             processComponents(catalog, artifacts);
@@ -113,13 +111,6 @@ public class GenerateCatalogMojo extends AbstractMojo {
                 p.process(project, catalog, artifacts);
             });
 
-            DumperOptions options = new DumperOptions();
-            options.setIndent(2);
-            options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-
-            Representer representer = new CamelRepresenter();
-            representer.addClassTag(CamelArtifact.class, Tag.MAP);
-
             //
             // apiVersion: camel.apache.org/v1alpha1
             // kind: CamelCatalog
@@ -131,37 +122,41 @@ public class GenerateCatalogMojo extends AbstractMojo {
             //     camel.apache.org/catalog.loader.version: x.y.z
             // spec:
             //   version:
+            // status:
             //   artifacts:
             //
             try (Writer writer = Files.newBufferedWriter(output, StandardCharsets.UTF_8)) {
-                Map<String, Object> cr = new LinkedHashMap<>();
-                cr.put("apiVersion", "camel.apache.org/v1alpha1");
-                cr.put("kind", "CamelCatalog");
+                CamelCatalog cr = new CamelCatalog.Builder()
+                    .metadata(new ObjectMeta.Builder()
+                        .name("camel-catalog-" + catalog.getCatalogVersion())
+                        .putLabels("app", "camel-k")
+                        .putLabels("camel.apache.org/catalog.version", catalog.getCatalogVersion())
+                        .putLabels("camel.apache.org/catalog.loader.version", catalog.getLoadedVersion())
+                        .build())
+                    .spec(new CamelCatalogSpec.Builder()
+                        .version(catalog.getCatalogVersion())
+                        .artifacts(artifacts)
+                        .build())
+                    .build();
 
-                Map<String, Object> labels = new LinkedHashMap<>();
-                labels.put("app", "camel-k");
-                labels.put("camel.apache.org/catalog.version", catalog.getCatalogVersion());
-                labels.put("camel.apache.org/catalog.loader.version", catalog.getLoadedVersion());
+                YAMLFactory factory = new YAMLFactory()
+                    .configure(YAMLGenerator.Feature.MINIMIZE_QUOTES, true)
+                    .configure(YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS, true)
+                    .configure(YAMLGenerator.Feature.USE_NATIVE_TYPE_ID, false)
+                    .configure(YAMLGenerator.Feature.WRITE_DOC_START_MARKER, false);
 
-                Map<String, Object> meta = new LinkedHashMap<>();
-                meta.put("name", "camel-catalog-" + catalog.getCatalogVersion());
-                meta.put("labels", labels);
-
-                Map<String, Object> spec = new LinkedHashMap<>();
-                spec.put("artifacts", artifacts);
-                spec.put("version", catalog.getCatalogVersion());
-
-                cr.put("metadata", meta);
-                cr.put("spec", spec);
-
-                new Yaml(representer, options).dump(cr, writer);
+                //new Yaml(representer, options).dump(cr, writer);
+                ObjectMapper mapper = new ObjectMapper(factory);
+                mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+                mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+                mapper.writeValue(writer, cr);
             }
         } catch (IOException e) {
             throw new MojoExecutionException("Exception while generating catalog", e);
         }
     }
 
-    private void processComponents(CamelCatalog catalog, Map<String, CamelArtifact> artifacts) {
+    private void processComponents(org.apache.camel.catalog.CamelCatalog catalog, Map<String, CamelArtifact> artifacts) {
         for (String name : catalog.findComponentNames()) {
             String json = catalog.componentJSonSchema(name);
             CatalogComponentDefinition definition = CatalogSupport.unmarshallComponent(json);
@@ -186,7 +181,7 @@ public class GenerateCatalogMojo extends AbstractMojo {
         }
     }
 
-    private void processLanguages(CamelCatalog catalog, Map<String, CamelArtifact> artifacts) {
+    private void processLanguages(org.apache.camel.catalog.CamelCatalog catalog, Map<String, CamelArtifact> artifacts) {
         for (String name : catalog.findLanguageNames()) {
             String json = catalog.languageJSonSchema(name);
             CatalogLanguageDefinition definition = CatalogSupport.unmarshallLanguage(json);
@@ -208,7 +203,7 @@ public class GenerateCatalogMojo extends AbstractMojo {
         }
     }
 
-    private void processDataFormats(CamelCatalog catalog, Map<String, CamelArtifact> artifacts) {
+    private void processDataFormats(org.apache.camel.catalog.CamelCatalog catalog, Map<String, CamelArtifact> artifacts) {
         for (String name : catalog.findDataFormatNames()) {
             String json = catalog.dataFormatJSonSchema(name);
             CatalogDataFormatDefinition definition = CatalogSupport.unmarshallDataFormat(json);
@@ -227,31 +222,6 @@ public class GenerateCatalogMojo extends AbstractMojo {
 
                 return artifact;
             });
-        }
-    }
-
-    // *************************
-    //
-    // Helpers
-    //
-    // *************************
-
-    private static class CamelRepresenter extends Representer {
-        @Override
-        protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue, Tag customTag) {
-            NodeTuple tuple = super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
-
-            Node valueNode = tuple.getValueNode();
-            if (Tag.NULL.equals(valueNode.getTag())) {
-                return null;
-            }
-            if (valueNode instanceof CollectionNode) {
-                CollectionNode col = (CollectionNode) valueNode;
-                if (col.getValue().isEmpty()) {
-                    return null;
-                }
-            }
-            return tuple;
         }
     }
 }
