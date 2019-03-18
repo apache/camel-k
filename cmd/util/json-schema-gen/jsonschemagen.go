b7d849a62a5a36ac2a1d0286bd7b28268303a8e5
lburgazzoli <lburgazzoli@gmail.com>
2019-03-18 16:05:07 +0100

    chore: update resources

--- cmd/util/json-schema-gen/jsonschemagen.go
+++ cmd/util/json-schema-gen/jsonschemagen.go
@@ -34,7 +34,7 @@ func main() {
 	sources := []interface{}{
 		v1alpha1.Integration{},
 		v1alpha1.IntegrationContext{},
-		v1alpha1.IntegrationPlatform{},
+		//v1alpha1.IntegrationPlatform{},
 		v1alpha1.CamelCatalog{},
 	}
 
