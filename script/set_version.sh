f7aac4d55979bf63421ee1c3a35c8b39a5b5039e
Andrea Tarocchi <atarocch@redhat.com>
2019-04-10 23:54:20 +0200

    fix #602: Script set-version.sh does not honor the registry/repository settings.

--- script/set_version.sh
+++ script/set_version.sh
@@ -9,10 +9,13 @@ fi
 
 location=$(dirname $0)
 version=$1
+image_name=${2:-docker.io\/apache\/camel-k}
+sanitized_image_name=${image_name/\//\\\/}
+
 
 for f in $(find $location/../deploy -type f -name "*.yaml"); 
 do
-    sed -i -r "s/docker.io\/apache\/camel-k:([0-9]+[a-zA-Z0-9\-\.].*).*/docker.io\/apache\/camel-k:${version}/" $f
+    sed -i -r "s/docker.io\/apache\/camel-k:([0-9]+[a-zA-Z0-9\-\.].*).*/${sanitized_image_name}:${version}/" $f
 done
 
-echo "Camel K version set to: $version"
+echo "Camel K version set to: $version and image name to: $image_name"
