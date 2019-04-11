c50aebf37230493bb77d3d858334656e81962640
Claus Ibsen <claus.ibsen@gmail.com>
2019-04-11 13:03:12 +0200

    Lets make the sample.java even simpler

--- examples/Sample.java
+++ examples/Sample.java
@@ -4,7 +4,6 @@ public class Sample extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 	  from("timer:tick")
-    .setBody(constant("-\n             r\n             o\n             c\nHello! Camel K\n             s\n             !\n"))
-		.to("log:info?skipBodyLineSeparator=false");
+      .log("Hello Camel K!");
   }
 }
