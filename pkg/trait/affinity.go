3d2282a9539486997bca4201ded7d6ffc1b27f81
Antonin Stefanutti <antonin@stefanutti.fr>
2019-04-12 12:36:52 +0200

    Use matching expressions instead of labels to configure inter-pod affinity and anti-affinity

--- pkg/trait/affinity.go
+++ pkg/trait/affinity.go
@@ -50,21 +50,14 @@ func (t *affinityTrait) Configure(e *Environment) (bool, error) {
 		return false, nil
 	}
 
+	if t.PodAffinity && t.PodAntiAffinity {
+		return false, fmt.Errorf("both pod affinity and pod anti-affinity can't be set simultaneously")
+	}
+
 	return e.IntegrationInPhase(v1alpha1.IntegrationPhaseDeploying), nil
 }
 
 func (t *affinityTrait) Apply(e *Environment) (err error) {
-	if t.NodeAffinityLabels == "" &&
-		!t.PodAffinity && t.PodAffinityLabels == "" &&
-		!t.PodAntiAffinity && t.PodAntiAffinityLabels == "" {
-		// Nothing to do
-		return nil
-	}
-
-	if t.PodAffinity && t.PodAntiAffinity {
-		return fmt.Errorf("both pod affinity and pod anti-affinity can't be set simultaneously")
-	}
-
 	var deployment *appsv1.Deployment
 	e.Resources.VisitDeployment(func(d *appsv1.Deployment) {
 		if d.Name == e.Integration.Name {
@@ -72,7 +65,6 @@ func (t *affinityTrait) Apply(e *Environment) (err error) {
 		}
 	})
 	if deployment != nil {
-		deployment.Spec.Template.Spec.Affinity = &corev1.Affinity{}
 		if err := t.addNodeAffinity(e, deployment); err != nil {
 			return err
 		}
@@ -88,13 +80,12 @@ func (t *affinityTrait) Apply(e *Environment) (err error) {
 }
 
 func (t *affinityTrait) addNodeAffinity(_ *Environment, deployment *appsv1.Deployment) error {
-	nodeAffinityLabels := t.NodeAffinityLabels
-	if nodeAffinityLabels == "" {
+	if t.NodeAffinityLabels == "" {
 		return nil
 	}
 
 	nodeSelectorRequirements := make([]corev1.NodeSelectorRequirement, 0)
-	selector, err := labels.Parse(nodeAffinityLabels)
+	selector, err := labels.Parse(t.NodeAffinityLabels)
 	if err != nil {
 		return err
 	}
@@ -122,6 +113,10 @@ func (t *affinityTrait) addNodeAffinity(_ *Environment, deployment *appsv1.Deplo
 		},
 	}
 
+	if deployment.Spec.Template.Spec.Affinity == nil {
+		deployment.Spec.Template.Spec.Affinity = &corev1.Affinity{}
+	}
+
 	deployment.Spec.Template.Spec.Affinity.NodeAffinity = nodeAffinity
 
 	return nil
@@ -132,30 +127,50 @@ func (t *affinityTrait) addPodAffinity(e *Environment, deployment *appsv1.Deploy
 		return nil
 	}
 
-	matchLabels := make(map[string]string)
+	labelSelectorRequirements := make([]metav1.LabelSelectorRequirement, 0)
 	if t.PodAffinityLabels != "" {
-		labelsMap, err := labels.ConvertSelectorToLabelsMap(t.PodAffinityLabels)
+		selector, err := labels.Parse(t.PodAffinityLabels)
 		if err != nil {
 			return err
 		}
-		matchLabels = labelsMap
+		requirements, _ := selector.Requirements()
+		for _, r := range requirements {
+			operator, err := operatorToLabelSelectorOperator(r.Operator())
+			if err != nil {
+				return err
+			}
+			requirement := metav1.LabelSelectorRequirement{
+				Key:      r.Key(),
+				Operator: operator,
+				Values:   r.Values().List(),
+			}
+			labelSelectorRequirements = append(labelSelectorRequirements, requirement)
+		}
 	}
 
 	if t.PodAffinity {
-		if _, ok := matchLabels["camel.apache.org/integration"]; !ok {
-			matchLabels["camel.apache.org/integration"] = e.Integration.Name
-		}
+		labelSelectorRequirements = append(labelSelectorRequirements, metav1.LabelSelectorRequirement{
+			Key:      "camel.apache.org/integration",
+			Operator: metav1.LabelSelectorOpIn,
+			Values: []string{
+				e.Integration.Name,
+			},
+		})
 	}
 
 	podAffinity := &corev1.PodAffinity{
 		RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
 			{
 				LabelSelector: &metav1.LabelSelector{
-					MatchLabels: matchLabels,
+					MatchExpressions: labelSelectorRequirements,
 				},
 				TopologyKey: "kubernetes.io/hostname",
 			},
 		},
+	}
+
+	if deployment.Spec.Template.Spec.Affinity == nil {
+		deployment.Spec.Template.Spec.Affinity = &corev1.Affinity{}
 	}
 
 	deployment.Spec.Template.Spec.Affinity.PodAffinity = podAffinity
@@ -168,30 +183,50 @@ func (t *affinityTrait) addPodAntiAffinity(e *Environment, deployment *appsv1.De
 		return nil
 	}
 
-	matchLabels := make(map[string]string)
+	labelSelectorRequirements := make([]metav1.LabelSelectorRequirement, 0)
 	if t.PodAntiAffinityLabels != "" {
-		labelsMap, err := labels.ConvertSelectorToLabelsMap(t.PodAntiAffinityLabels)
+		selector, err := labels.Parse(t.PodAntiAffinityLabels)
 		if err != nil {
 			return err
 		}
-		matchLabels = labelsMap
+		requirements, _ := selector.Requirements()
+		for _, r := range requirements {
+			operator, err := operatorToLabelSelectorOperator(r.Operator())
+			if err != nil {
+				return err
+			}
+			requirement := metav1.LabelSelectorRequirement{
+				Key:      r.Key(),
+				Operator: operator,
+				Values:   r.Values().List(),
+			}
+			labelSelectorRequirements = append(labelSelectorRequirements, requirement)
+		}
 	}
 
 	if t.PodAntiAffinity {
-		if _, ok := matchLabels["camel.apache.org/integration"]; !ok {
-			matchLabels["camel.apache.org/integration"] = e.Integration.Name
-		}
+		labelSelectorRequirements = append(labelSelectorRequirements, metav1.LabelSelectorRequirement{
+			Key:      "camel.apache.org/integration",
+			Operator: metav1.LabelSelectorOpIn,
+			Values: []string{
+				e.Integration.Name,
+			},
+		})
 	}
 
 	podAntiAffinity := &corev1.PodAntiAffinity{
 		RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
 			{
 				LabelSelector: &metav1.LabelSelector{
-					MatchLabels: matchLabels,
+					MatchExpressions: labelSelectorRequirements,
 				},
 				TopologyKey: "kubernetes.io/hostname",
 			},
 		},
+	}
+
+	if deployment.Spec.Template.Spec.Affinity == nil {
+		deployment.Spec.Template.Spec.Affinity = &corev1.Affinity{}
 	}
 
 	deployment.Spec.Template.Spec.Affinity.PodAntiAffinity = podAntiAffinity
@@ -201,9 +236,9 @@ func (t *affinityTrait) addPodAntiAffinity(e *Environment, deployment *appsv1.De
 
 func operatorToNodeSelectorOperator(operator selection.Operator) (corev1.NodeSelectorOperator, error) {
 	switch operator {
-	case selection.In:
+	case selection.In, selection.Equals, selection.DoubleEquals:
 		return corev1.NodeSelectorOpIn, nil
-	case selection.NotIn:
+	case selection.NotIn, selection.NotEquals:
 		return corev1.NodeSelectorOpNotIn, nil
 	case selection.Exists:
 		return corev1.NodeSelectorOpExists, nil
@@ -215,4 +250,18 @@ func operatorToNodeSelectorOperator(operator selection.Operator) (corev1.NodeSel
 		return corev1.NodeSelectorOpLt, nil
 	}
 	return "", fmt.Errorf("unsupported node selector operator: %s", operator)
+}
+
+func operatorToLabelSelectorOperator(operator selection.Operator) (metav1.LabelSelectorOperator, error) {
+	switch operator {
+	case selection.In, selection.Equals, selection.DoubleEquals:
+		return metav1.LabelSelectorOpIn, nil
+	case selection.NotIn, selection.NotEquals:
+		return metav1.LabelSelectorOpNotIn, nil
+	case selection.Exists:
+		return metav1.LabelSelectorOpExists, nil
+	case selection.DoesNotExist:
+		return metav1.LabelSelectorOpDoesNotExist, nil
+	}
+	return "", fmt.Errorf("unsupported label selector operator: %s", operator)
 }
