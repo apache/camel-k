6b6ef491f0f2fca44d6651b2029f9c609c96a144
stevef1uk <stevef1uk@users.noreply.github.com>
2019-04-17 23:25:13 +0100

    Update Sample_Kafka_Consumer.java

--- examples/Sample_Kafka_Consumer.java
+++ examples/Sample_Kafka_Consumer.java
@@ -1,13 +1,13 @@
 /* To run this example first set-up Kafka. If using minikube follow these instructions:
-# https://strimzi.io/quickstarts/minikube/
-# Create a configmap to contain the properties e.g.
-# kubectl create configmap kafka.props  --from-file=application.properties
-# Note: The kafka.host value in application.properties needs to be set to the CLUSTER-IP address of the my-cluster-kafka-bootstrap service in the kafka namespace
-# e.g. kubectl get services -n kafka | grep my-cluster-kafka-bootstrap | awk '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ {print $3}'
-# Run this sample using the commpand:
-# kamel run examples/Sample_Kafka_Consumer.java --configmap=kafka.props
-# To create messages to be read use the producer commanf from the Strimzi page e.g.
-# kubectl -n kafka run kafka-producer -ti --image=strimzi/kafka:0.11.1-kafka-2.1.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list my-cluster-kafka-bootstrap:9092 --topic my-topic
+    https://strimzi.io/quickstarts/minikube/
+    Create a configmap to contain the properties e.g.
+      kubectl create configmap kafka.props  --from-file=examples/application.properties
+      Note: The kafka.host value in application.properties needs to be set to the CLUSTER-IP address of the my-cluster-kafka-bootstrap service in the kafka namespace
+        e.g. kubectl get services -n kafka | grep my-cluster-kafka-bootstrap | awk '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ {print $3}'
+    Run this sample using the command:
+      kamel run examples/Sample_Kafka_Consumer.java --configmap=kafka.props
+    To create messages to be read use the producer commanf from the Strimzi page e.g.
+      kubectl -n kafka run kafka-producer -ti --image=strimzi/kafka:0.11.1-kafka-2.1.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list my-cluster-kafka-bootstrap:9092 --topic my-topic
 */
 
 import org.apache.camel.builder.RouteBuilder;
