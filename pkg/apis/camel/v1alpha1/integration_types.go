83f48d676261b7a63061a66bf2e98ca99bc4b8fc
lburgazzoli <lburgazzoli@gmail.com>
2019-05-21 15:01:16 +0200

    chore: support for health based on servlet available from 0.3.3-SNAPSHOT

--- pkg/apis/camel/v1alpha1/integration_types.go
+++ pkg/apis/camel/v1alpha1/integration_types.go
@@ -22,15 +22,16 @@ type IntegrationSpec struct {
 
 // IntegrationStatus defines the observed state of Integration
 type IntegrationStatus struct {
-	Phase            IntegrationPhase `json:"phase,omitempty"`
-	Digest           string           `json:"digest,omitempty"`
-	Image            string           `json:"image,omitempty"`
-	Dependencies     []string         `json:"dependencies,omitempty"`
-	Context          string           `json:"context,omitempty"`
-	GeneratedSources []SourceSpec     `json:"generatedSources,omitempty"`
-	Failure          *Failure         `json:"failure,omitempty"`
-	CamelVersion     string           `json:"camelVersion,omitempty"`
-	RuntimeVersion   string           `json:"runtimeVersion,omitempty"`
+	Phase            IntegrationPhase    `json:"phase,omitempty"`
+	Digest           string              `json:"digest,omitempty"`
+	Image            string              `json:"image,omitempty"`
+	Dependencies     []string            `json:"dependencies,omitempty"`
+	Context          string              `json:"context,omitempty"`
+	GeneratedSources []SourceSpec        `json:"generatedSources,omitempty"`
+	Failure          *Failure            `json:"failure,omitempty"`
+	CamelVersion     string              `json:"camelVersion,omitempty"`
+	RuntimeVersion   string              `json:"runtimeVersion,omitempty"`
+	Configuration    []ConfigurationSpec `json:"configuration,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
