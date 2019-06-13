a5ea7cf5b1236ccfd1c882af3f9cfa5a9030f22b
nferraro <ni.ferraro@gmail.com>
2019-06-13 16:06:54 +0200

    Fix #749: do not create route if not needed

--- pkg/trait/ingress.go
+++ pkg/trait/ingress.go
@@ -83,7 +83,10 @@ func (t *ingressTrait) getTargetService(e *Environment) (service *corev1.Service
 	e.Resources.VisitService(func(s *corev1.Service) {
 		if s.ObjectMeta.Labels != nil {
 			if intName, ok := s.ObjectMeta.Labels["camel.apache.org/integration"]; ok && intName == e.Integration.Name {
-				service = s
+				if s.ObjectMeta.Labels["camel.apache.org/service.type"] == "user" {
+					// We should build an ingress only on top of the user service (e.g. not if the service contains only prometheus)
+					service = s
+				}
 			}
 		}
 	})
