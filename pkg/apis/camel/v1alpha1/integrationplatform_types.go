07e87c38caef957a5e4d43b4b755f7d8685ab902
James Netherton <jamesnetherton@gmail.com>
2019-08-13 09:27:47 +0100

    fix: Enable proxy server to be configured for Kaniko builds
    fixes #899

--- pkg/apis/camel/v1alpha1/integrationplatform_types.go
+++ pkg/apis/camel/v1alpha1/integrationplatform_types.go
@@ -104,6 +104,7 @@ type IntegrationPlatformBuildSpec struct {
 	Timeout               metav1.Duration                         `json:"timeout,omitempty"`
 	PersistentVolumeClaim string                                  `json:"persistentVolumeClaim,omitempty"`
 	Maven                 MavenSpec                               `json:"maven,omitempty"`
+	HTTPProxySecret       string                                  `json:"httpProxySecret,omitempty"`
 }
 
 // IntegrationPlatformRegistrySpec --
