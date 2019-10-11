465444d0234bd72ce9cd8a06e1571a5d236bd594
nferraro <ni.ferraro@gmail.com>
2019-10-11 15:10:17 +0200

    fix #1005: fix findings

--- pkg/apis/camel/v1alpha1/integration_types.go
+++ pkg/apis/camel/v1alpha1/integration_types.go
@@ -109,7 +109,7 @@ type SourceSpec struct {
 	Language Language `json:"language,omitempty"`
 	// Loader is an optional id of the org.apache.camel.k.RoutesLoader that will
 	// interpret this source at runtime
-	Loader   string   `json:"loader,omitempty"`
+	Loader string `json:"loader,omitempty"`
 }
 
 // Language --
