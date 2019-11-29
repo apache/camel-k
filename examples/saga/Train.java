d706ea15910936b1889231fff6ec8551daf94584
Amos Feng <zfeng@redhat.com>
2019-11-29 13:53:28 +0800

    Update to add the saga example

---
+++ examples/saga/Train.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.SagaPropagation;
+import org.apache.camel.model.rest.RestParamType;
+import org.apache.camel.service.lra.LRASagaService;
+import org.apache.camel.Exchange;
+
+public class Train extends RouteBuilder {
+	@Override
+	public void configure() throws Exception {
+                restConfiguration().port("8080");
+
+		LRASagaService service = new LRASagaService();
+		service.setCoordinatorUrl("http://lra-coordinator:8080");
+		service.setLocalParticipantUrl("http://train:8080");
+		getContext().addService(service);
+
+		rest("/api/").post("/train/buy/seat")
+                    .param().type(RestParamType.header).name("id").required(true).endParam()
+                    .route()
+                    .saga()
+                        .propagation(SagaPropagation.SUPPORTS)
+                        .option("id", header("id"))
+                        .compensation("direct:cancelPurchase")
+                    .log("Buying train seat #${header.id}")
+                    .to("http://payment:8080/api/pay?bridgeEndpoint=true&type=train")
+                    .log("Payment for train #${header.id} done");
+
+               from("direct:cancelPurchase")
+                    .log("Train purchase #${header.id} has been cancelled");
+	}
+}
