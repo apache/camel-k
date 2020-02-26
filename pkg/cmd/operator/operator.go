66953d7b3a9e24b3e3e87974b8af96aefa0909b5
Antonin Stefanutti <antonin@stefanutti.fr>
2020-02-26 10:59:08 +0100

    chore: Display Buildah version in operator prelude

--- pkg/cmd/operator/operator.go
+++ pkg/cmd/operator/operator.go
@@ -56,6 +56,7 @@ func printVersion() {
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
 	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
 	log.Info(fmt.Sprintf("Operator SDK Version: %v", sdkVersion.Version))
+	log.Info(fmt.Sprintf("Buildah Version: %v", defaults.BuildahVersion))
 	log.Info(fmt.Sprintf("Kaniko Version: %v", defaults.KanikoVersion))
 	log.Info(fmt.Sprintf("Camel K Operator Version: %v", defaults.Version))
 	log.Info(fmt.Sprintf("Camel K Git Commit: %v", GitCommit))
