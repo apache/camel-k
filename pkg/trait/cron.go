3f615b5c8abd78300e9e4dd10ad75c211a263238
Nicola Ferraro <ni.ferraro@gmail.com>
2020-03-17 18:17:04 +0100

    Fix #1350: remove other places where the timer human readable format is used

--- pkg/trait/cron.go
+++ pkg/trait/cron.go
@@ -45,7 +45,7 @@ import (
 // Integrations that start from the following components are evaluated by the cron trait: `timer`, `cron`, `quartz`.
 //
 // The rules for using a Kubernetes CronJob are the following:
-// - `timer`: when periods can be written as cron expressions. E.g. `timer:tick?period=1m`.
+// - `timer`: when periods can be written as cron expressions. E.g. `timer:tick?period=60000`.
 // - `cron`, `quartz`: when the cron expression does not contain seconds (or the "seconds" part is set to 0). E.g.
 //   `cron:tab?schedule=0/2+*+*+*+?` or `quartz:trigger?cron=0+0/2+*+*+*+?`.
 //
