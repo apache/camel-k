278d1babbbe6e626ed8a29e18ed9d82eea464d27
Nicola Ferraro <ni.ferraro@gmail.com>
2020-03-20 16:32:08 +0100

    chore(build): bump runtime to 1.2.1

--- pkg/util/defaults/defaults.go
+++ pkg/util/defaults/defaults.go
@@ -26,7 +26,7 @@ const (
 	Version = "1.0.0-SNAPSHOT"
 
 	// DefaultRuntimeVersion --
-	DefaultRuntimeVersion = "1.2.0-SNAPSHOT"
+	DefaultRuntimeVersion = "1.2.1"
 
 	// BuildahVersion --
 	BuildahVersion = "1.14.0"
