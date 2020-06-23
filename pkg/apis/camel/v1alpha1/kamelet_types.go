fd45cd66fb0e460a2aaf7d727807288b7987fa81
Nicola Ferraro <ni.ferraro@gmail.com>
2020-06-23 21:26:41 +0200

    kamelets: improving kamelet format

--- pkg/apis/camel/v1alpha1/kamelet_types.go
+++ pkg/apis/camel/v1alpha1/kamelet_types.go
@@ -1,53 +1,54 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha1
 
 import (
 	camelv1 "github.com/apache/camel-k/pkg/apis/camel/v1"
-	openapi "github.com/go-openapi/spec"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	AnnotationIcon = "camel.apache.org/icon"
+)
+
 // KameletSpec defines the desired state of Kamelet
 type KameletSpec struct {
-	Info          KameletInfo         `json:"info,omitempty"`
-	Parameters    []KameletParameter  `json:"parameters,omitempty"`
-	Authorization AuthorizationSpec   `json:"authorization,omitempty"`
-	Sources       *camelv1.SourceSpec `json:"sources,omitempty"`
-	Flow          *camelv1.Flow       `json:"flow,omitempty"`
-	Consumes      *EventType          `json:"consumes,omitempty"`
-	Produces      *EventType          `json:"produces,omitempty"`
-	Dependencies  []string            `json:"dependencies,omitempty"`
+	Definition    JSONSchemaProps             `json:"definition,omitempty"`
+	Sources       *camelv1.SourceSpec         `json:"sources,omitempty"`
+	Flow          *camelv1.Flow               `json:"flow,omitempty"`
+	Authorization AuthorizationSpec           `json:"authorization,omitempty"`
+	Types         map[EventSlot]EventTypeSpec `json:"types,omitempty"`
+	Dependencies  []string                    `json:"dependencies,omitempty"`
 }
 
-type EventType struct {
-	Type   string      `json:"type,omitempty"`
-	Schema *DataSchema `json:"schema,omitempty"`
+type EventTypeSpec struct {
+	MediaType string           `json:"mediaType,omitempty"`
+	Schema    *JSONSchemaProps `json:"schema,omitempty"`
 }
 
-type DataSchema struct {
-	Ref     *corev1.ObjectReference `json:"ref,omitempty"`
-	Content *openapi.Schema         `json:"schema,omitempty"`
-}
+type EventSlot string
 
-type KameletInfo struct {
-	DisplayName string      `json:"displayName,omitempty"`
-	Description string      `json:"description,omitempty"`
-	Group       string      `json:"group,omitempty"`
-	Icon        KameletIcon `json:"icon,omitempty"`
-}
-
-type KameletIcon struct {
-	Data      string `json:"data,omitempty"`
-	MediaType string `json:"mediaType,omitempty"`
-}
-
-type KameletParameter struct {
-	Name        string            `json:"name,omitempty"`
-	Required    bool              `json:"required,omitempty"`
-	Description string            `json:"description,omitempty"`
-	Annotations map[string]string `json:"annotations,omitempty"`
-	Schema      *openapi.Schema   `json:"schema,omitempty"`
-}
+const (
+	EventSlotIn    EventSlot = "in"
+	EventSlotOut   EventSlot = "out"
+	EventSlotError EventSlot = "error"
+)
 
 // AuthorizationSpec is TODO (oauth information)
 type AuthorizationSpec struct {
