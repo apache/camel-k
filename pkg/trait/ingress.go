e0e802395ce892257adcbf4d810feb62cabcf1e7
Antonin Stefanutti <antonin@stefanutti.fr>
2020-07-15 15:11:56 +0200

    chore(trait): Migrate the Ingress trait to structured serialization

--- pkg/trait/ingress.go
+++ pkg/trait/ingress.go
@@ -39,9 +39,9 @@ import (
 type ingressTrait struct {
 	BaseTrait `property:",squash"`
 	// **Required**. To configure the host exposed by the ingress.
-	Host string `property:"host"`
+	Host string `property:"host" json:"host,omitempty"`
 	// To automatically add an ingress whenever the integration uses a HTTP endpoint consumer.
-	Auto *bool `property:"auto"`
+	Auto *bool `property:"auto" json:"auto,omitempty"`
 }
 
 func newIngressTrait() Trait {
