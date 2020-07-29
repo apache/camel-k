713e3f75e94a680fbd9e4d6d84931724b0bd0ee7
Aleksander Slominski <aslom@us.ibm.com>
2020-07-29 16:43:07 -0400

    Add support for oidc (#1647)
    Fixes https://github.com/apache/camel-k/issues/1646

--- cmd/kamel/main.go
+++ cmd/kamel/main.go
@@ -30,6 +30,7 @@ import (
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
 func main() {
