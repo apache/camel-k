3a4f560037325b93080ec631f7301a38c9359603
Lukas Lowinger <llowinge@redhat.com>
2020-09-30 10:03:26 +0200

    [fix] Use in-place editing

--- e2e/yaks/monitoring/obtainToken.sh
+++ e2e/yaks/monitoring/obtainToken.sh
@@ -4,4 +4,4 @@ SOURCE_DIR=$( dirname "${BASH_SOURCE[0]}")
 TEST_FILE="${SOURCE_DIR}/metrics.feature"
 
 TOKEN=`kubectl config view --minify --output 'jsonpath={..token}'`
-sed "s/TOKEN/${TOKEN}/g" "${TEST_FILE}"
+sed -i -e "s/TOKEN/${TOKEN}/g" "${TEST_FILE}"
