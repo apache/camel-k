795ce906ed7884bf49f046c3c89dc096252eaae4
Antonin Stefanutti <antonin@stefanutti.fr>
2020-10-06 10:35:07 +0200

    chore: Upgrade Prometheus to version 0.42.1

--- pkg/trait/prometheus.go
+++ pkg/trait/prometheus.go
@@ -27,7 +27,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 
 	"github.com/apache/camel-k/deploy"
 	v1 "github.com/apache/camel-k/pkg/apis/camel/v1"
--- pkg/trait/prometheus_test.go
+++ pkg/trait/prometheus_test.go
@@ -23,11 +23,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 
 	v1 "github.com/apache/camel-k/pkg/apis/camel/v1"
 	"github.com/apache/camel-k/pkg/util/camel"
