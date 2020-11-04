ec86a9ccfa60132c2e572b57dcfeaddeed5e924d
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2020-11-04 14:05:25 +0000

    #1743: Begin generating the operator bundle using operator-sdk
    * Makefile
     * OPERATOR_VERSION removes -SNAPSHOT suffix as invalid domain for version
       field while executing `operator-sdk generate...`
     * Adds recipes for constructing both the manifests and bundle, adapted
       from the demonstration versions
     * Uses recursive rule-making to avoid duplication and work around
       the limits of controller-gen & operator-sdk
    
    * script/add_licence.sh
     * Script for prepending the licence header to generated files
    
    * script/gen_crd.sh
     * adds config directory for crd generation
    
    * PROJECT
     * Scaffolding configuration for operator-sdk executable
    
    * config
     * root configuration directory for kustomize and operator-sdk
     * samples directory locates the alm-examples used in the bundle manifest
     * rbac directory locates the permissions, cluster-permissions and
       deployments fields in the bundle manifest
     * prometheus directory used for monitoring resources although doesn't seem
       to be applicable at the moment (prometheus-jmx-exporter.yaml is invalid)
     * crd directory locates the CRDs generated in the same manner as that in
       the deploy directory
     * All resources have to be copied in - cannot use symlinks and both
       controller-gen and operator-sdk will error
    
    * Limitations of process:
     * controller-gen cannot generate CRDs across modules (directory containing
       go.mod) so gen-crds.sh changes directory into pkg/apis in order to
       execute successfully
     * operator-sdk has to execute in root of directory and cannot generate
       across modules. Also, seems to have a duplication problem if 2 apis
       are being processed with one dependent on the other. So copy apis 1 at
       a time & process
    
    * Result is `make bundle` will regenerate csv manifests (if required) then
      create a bundle directory containing these manifests and other artifacts
      necessary for deploying an image for the Operator Hub and OLM catalogue

---
+++ script/add_license.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+location=$(dirname $0)
+rootdir=$location/../
+
+cd $rootdir
+go build ./cmd/util/license-check/
+
+#
+# Requires directory to find the files in
+# The header file containing the header text
+#
+
+dir=$1
+header=$2
+
+if [ ! -d "$dir" ]; then
+  echo "Error: Cannot find directory."
+	exit 1
+fi
+
+if [ ! -f "$header" ]; then
+	echo "Error: Cannot find header file."
+	exit 1
+fi
+
+set +e
+failed=0
+find "$dir" -type f -print0 | while IFS= read -r -d '' file; do
+  ./license-check "$file" "$header" &> /dev/null
+	if [ $? -ne 0 ]; then
+		cat "$header" <(echo) "$file" > "${file}.new"
+		if [ $? -eq 0 ]; then
+			mv "${file}.new" "${file}"
+		fi
+	fi
+done
+set -e
+
+#if [ $failed -ne 0 ]; then
+#  exit 1
+#fi
