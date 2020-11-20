ec4ed02e1a155a6f9ccdec882ce30ff222432934
Doru Bercea <gheorghe-teod.bercea@ibm.com>
2020-11-20 17:08:24 -0500

    Add support for creating integration image.

--- pkg/cmd/local_create.go
+++ pkg/cmd/local_create.go
@@ -38,13 +38,13 @@ func newCmdLocalCreate(rootCmdOptions *RootCmdOptions) (*cobra.Command, *localCr
 			if err := options.validate(args); err != nil {
 				return err
 			}
-			if err := options.init(); err != nil {
+			if err := options.init(args); err != nil {
 				return err
 			}
 			if err := options.run(args); err != nil {
 				fmt.Println(err.Error())
 			}
-			if err := options.deinit(); err != nil {
+			if err := options.deinit(args); err != nil {
 				return err
 			}
 
@@ -57,27 +57,67 @@ func newCmdLocalCreate(rootCmdOptions *RootCmdOptions) (*cobra.Command, *localCr
 
 	cmd.Flags().Bool("base-image", false, "Create base image used as a starting point for any integration.")
 	cmd.Flags().String("docker-registry", "", "Docker registry to store intermediate images.")
+	cmd.Flags().StringArray("property-file", nil, "Add a property file to the integration.")
+	cmd.Flags().StringArrayP("dependency", "d", nil, "Add an additional dependency")
 
 	return &cmd, &options
 }
 
 type localCreateCmdOptions struct {
 	*RootCmdOptions
-	BaseImage      bool   `mapstructure:"base-image"`
-	DockerRegistry string `mapstructure:"docker-registry"`
+	BaseImage              bool     `mapstructure:"base-image"`
+	DockerRegistry         string   `mapstructure:"docker-registry"`
+	AdditionalDependencies []string `mapstructure:"dependencies"`
+	PropertyFiles          []string `mapstructure:"property-files"`
 }
 
 func (command *localCreateCmdOptions) validate(args []string) error {
-	// If containerize is set then docker registry must be set.
-	if command.BaseImage && command.DockerRegistry == "" {
+	// Validate integration files.
+	if len(args) > 0 {
+		err := validateIntegrationFiles(args)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Validate additional dependencies specified by the user.
+	err := validateAdditionalDependencies(command.AdditionalDependencies)
+	if err != nil {
+		return err
+	}
+
+	// Validate properties file.
+	err = validateFiles(command.PropertyFiles)
+	if err != nil {
+		return nil
+	}
+
+	// Docker registry must be set.
+	if command.DockerRegistry == "" {
 		return errors.New("base image cannot be created as no registry has been provided")
 	}
 
 	return nil
 }
 
-func (command *localCreateCmdOptions) init() error {
-	return createDockerBaseWorkingDirectory()
+func (command *localCreateCmdOptions) init(args []string) error {
+	// If base image construction is enabled create a directory for it.
+	if command.BaseImage {
+		err := createDockerBaseWorkingDirectory()
+		if err != nil {
+			return err
+		}
+	}
+
+	// If integration files are provided an integration image will be built.
+	if len(args) > 0 {
+		err := createMavenWorkingDirectory()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (command *localCreateCmdOptions) run(args []string) error {
@@ -89,9 +129,37 @@ func (command *localCreateCmdOptions) run(args []string) error {
 		}
 	}
 
+	// Create integration image if integration files were provided.
+	if len(args) > 0 {
+		// Fetch dependencies.
+		dependencies, err := getDependencies(args, command.AdditionalDependencies, true)
+		if err != nil {
+			return err
+		}
+
+		// Get integration run command.
+		cmd := GetIntegrationRunCommand(command.PropertyFiles, dependencies, args)
+
+		// Run integration locally.
+		err = cmd.Run()
+		if err != nil {
+			return nil
+		}
+	}
+
 	return nil
 }
 
-func (command *localCreateCmdOptions) deinit() error {
-	return deleteDockerBaseWorkingDirectory()
+func (command *localCreateCmdOptions) deinit(args []string) error {
+	// If base image construction is enabled delete the directory for it.
+	if command.BaseImage {
+		deleteDockerBaseWorkingDirectory()
+	}
+
+	// If integration files are provided delete the maven project folder.
+	if len(args) > 0 {
+		deleteMavenWorkingDirectory()
+	}
+
+	return nil
 }
