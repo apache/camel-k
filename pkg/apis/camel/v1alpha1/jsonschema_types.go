359d792a0797fc0b4cfd1f211fddde6b3ee97b31
Antonin Stefanutti <antonin@stefanutti.fr>
2021-01-18 15:34:38 +0100

    chore(api): Remove protobuf tags from JSON schema types

--- pkg/apis/camel/v1alpha1/jsonschema_types.go
+++ pkg/apis/camel/v1alpha1/jsonschema_types.go
@@ -25,9 +25,9 @@ import (
 )
 
 type JSONSchemaProp struct {
-	ID          string `json:"id,omitempty" protobuf:"bytes,1,opt,name=id"`
-	Description string `json:"description,omitempty" protobuf:"bytes,4,opt,name=description"`
-	Type        string `json:"type,omitempty" protobuf:"bytes,5,opt,name=type"`
+	ID          string `json:"id,omitempty"`
+	Description string `json:"description,omitempty"`
+	Type        string `json:"type,omitempty"`
 	// format is an OpenAPI v3 format string. Unknown formats are ignored. The following formats are validated:
 	//
 	// - bsonobjectid: a bson object ID, i.e. a 24 characters hex string
@@ -54,39 +54,39 @@ type JSONSchemaProp struct {
 	// - date: a date string like "2006-01-02" as defined by full-date in RFC3339
 	// - duration: a duration string like "22 ns" as parsed by Golang time.ParseDuration or compatible with Scala duration format
 	// - datetime: a date time string like "2014-12-15T19:30:20.000Z" as defined by date-time in RFC3339.
-	Format string `json:"format,omitempty" protobuf:"bytes,6,opt,name=format"`
-	Title  string `json:"title,omitempty" protobuf:"bytes,7,opt,name=title"`
+	Format string `json:"format,omitempty"`
+	Title  string `json:"title,omitempty"`
 	// default is a default value for undefined object fields.
-	Default          *JSON        `json:"default,omitempty" protobuf:"bytes,8,opt,name=default"`
-	Maximum          *json.Number `json:"maximum,omitempty" protobuf:"bytes,9,opt,name=maximum"`
-	ExclusiveMaximum bool         `json:"exclusiveMaximum,omitempty" protobuf:"bytes,10,opt,name=exclusiveMaximum"`
-	Minimum          *json.Number `json:"minimum,omitempty" protobuf:"bytes,11,opt,name=minimum"`
-	ExclusiveMinimum bool         `json:"exclusiveMinimum,omitempty" protobuf:"bytes,12,opt,name=exclusiveMinimum"`
-	MaxLength        *int64       `json:"maxLength,omitempty" protobuf:"bytes,13,opt,name=maxLength"`
-	MinLength        *int64       `json:"minLength,omitempty" protobuf:"bytes,14,opt,name=minLength"`
-	Pattern          string       `json:"pattern,omitempty" protobuf:"bytes,15,opt,name=pattern"`
-	MaxItems         *int64       `json:"maxItems,omitempty" protobuf:"bytes,16,opt,name=maxItems"`
-	MinItems         *int64       `json:"minItems,omitempty" protobuf:"bytes,17,opt,name=minItems"`
-	UniqueItems      bool         `json:"uniqueItems,omitempty" protobuf:"bytes,18,opt,name=uniqueItems"`
-	MaxProperties    *int64       `json:"maxProperties,omitempty" protobuf:"bytes,21,opt,name=maxProperties"`
-	MinProperties    *int64       `json:"minProperties,omitempty" protobuf:"bytes,22,opt,name=minProperties"`
-	MultipleOf       *json.Number `json:"multipleOf,omitempty" protobuf:"bytes,19,opt,name=multipleOf"`
-	Enum             []JSON       `json:"enum,omitempty" protobuf:"bytes,20,rep,name=enum"`
-	Example          *JSON        `json:"example,omitempty" protobuf:"bytes,36,opt,name=example"`
-	Nullable         bool         `json:"nullable,omitempty" protobuf:"bytes,37,opt,name=nullable"`
+	Default          *JSON        `json:"default,omitempty"`
+	Maximum          *json.Number `json:"maximum,omitempty"`
+	ExclusiveMaximum bool         `json:"exclusiveMaximum,omitempty"`
+	Minimum          *json.Number `json:"minimum,omitempty"`
+	ExclusiveMinimum bool         `json:"exclusiveMinimum,omitempty"`
+	MaxLength        *int64       `json:"maxLength,omitempty"`
+	MinLength        *int64       `json:"minLength,omitempty"`
+	Pattern          string       `json:"pattern,omitempty"`
+	MaxItems         *int64       `json:"maxItems,omitempty"`
+	MinItems         *int64       `json:"minItems,omitempty"`
+	UniqueItems      bool         `json:"uniqueItems,omitempty"`
+	MaxProperties    *int64       `json:"maxProperties,omitempty"`
+	MinProperties    *int64       `json:"minProperties,omitempty"`
+	MultipleOf       *json.Number `json:"multipleOf,omitempty"`
+	Enum             []JSON       `json:"enum,omitempty"`
+	Example          *JSON        `json:"example,omitempty"`
+	Nullable         bool         `json:"nullable,omitempty"`
 }
 
 // JSONSchemaProps is a JSON-Schema following Specification Draft 4 (http://json-schema.org/).
 type JSONSchemaProps struct {
-	ID           string                    `json:"id,omitempty" protobuf:"bytes,1,opt,name=id"`
-	Description  string                    `json:"description,omitempty" protobuf:"bytes,4,opt,name=description"`
-	Title        string                    `json:"title,omitempty" protobuf:"bytes,7,opt,name=title"`
-	Properties   map[string]JSONSchemaProp `json:"properties,omitempty" protobuf:"bytes,29,rep,name=properties"`
-	Required     []string                  `json:"required,omitempty" protobuf:"bytes,23,rep,name=required"`
-	Example      *JSON                     `json:"example,omitempty" protobuf:"bytes,36,opt,name=example"`
-	ExternalDocs *ExternalDocumentation    `json:"externalDocs,omitempty" protobuf:"bytes,35,opt,name=externalDocs"`
-	Schema       JSONSchemaURL             `json:"$schema,omitempty" protobuf:"bytes,2,opt,name=schema"`
-	Type         string                    `json:"type,omitempty" protobuf:"bytes,5,opt,name=type"`
+	ID           string                    `json:"id,omitempty"`
+	Description  string                    `json:"description,omitempty"`
+	Title        string                    `json:"title,omitempty"`
+	Properties   map[string]JSONSchemaProp `json:"properties,omitempty"`
+	Required     []string                  `json:"required,omitempty"`
+	Example      *JSON                     `json:"example,omitempty"`
+	ExternalDocs *ExternalDocumentation    `json:"externalDocs,omitempty"`
+	Schema       JSONSchemaURL             `json:"$schema,omitempty"`
+	Type         string                    `json:"type,omitempty"`
 }
 
 // RawMessage is a raw encoded JSON value.
@@ -101,7 +101,7 @@ type RawMessage []byte
 // JSON represents any valid JSON value.
 // These types are supported: bool, int64, float64, string, []interface{}, map[string]interface{} and nil.
 type JSON struct {
-	RawMessage `json:",inline" protobuf:"bytes,1,opt,name=raw"`
+	RawMessage `json:",inline"`
 }
 
 // MarshalJSON returns m as the JSON encoding of m.
@@ -129,6 +129,6 @@ type JSONSchemaURL string
 
 // ExternalDocumentation allows referencing an external resource for extended documentation.
 type ExternalDocumentation struct {
-	Description string `json:"description,omitempty" protobuf:"bytes,1,opt,name=description"`
-	URL         string `json:"url,omitempty" protobuf:"bytes,2,opt,name=url"`
+	Description string `json:"description,omitempty""`
+	URL         string `json:"url,omitempty"`
 }
