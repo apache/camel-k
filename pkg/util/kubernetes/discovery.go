c9160df4fae2015eab6488f5db923963c602f30c
Antonin Stefanutti <antonin@stefanutti.fr>
2021-02-10 12:24:55 +0100

    fix: ServiceBinding SAR check does not guaranty CRD is installed

---
+++ pkg/util/kubernetes/discovery.go
@@ -0,0 +1,41 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubernetes
+
+import (
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/apache/camel-k/pkg/client"
+)
+func IsAPIResourceInstalled(c client.Client, groupVersion string, kind string) (bool, error) {
+	resources, err := c.Discovery().ServerResourcesForGroupVersion(groupVersion)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	for _, resource := range resources.APIResources {
+		if resource.Kind == kind {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
