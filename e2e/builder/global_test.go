5f5d6c0ba04766f4f378126ef7620ecab3462e2d
nicolaferraro <ni.ferraro@gmail.com>
2021-03-02 12:20:01 +0100

    Fix #1799: refactor test and regen

--- e2e/builder/global_test.go
+++ e2e/builder/global_test.go
@@ -28,9 +28,8 @@ import (
 	camelv1 "github.com/apache/camel-k/pkg/apis/camel/v1"
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/assert"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/apache/camel-k/e2e/support"
 	"github.com/apache/camel-k/pkg/platform"
@@ -51,83 +50,87 @@ func TestRunGlobalInstall(t *testing.T) {
 	WithNewTestNamespace(t, func(ns string) {
 		Expect(Kamel("install", "-n", ns, "--global").Execute()).To(Succeed())
 
-		// NS2: namespace without operator but with platform
-		WithNewTestNamespace(t, func(ns2 string) {
-			// Creating platform
-			Expect(Kamel("install", "-n", ns2, "--skip-operator-setup", "--olm=false").Execute()).To(Succeed())
+		t.Run("Global test on namespace with platform", func(t *testing.T) {
+			WithNewTestNamespace(t, func(ns2 string) {
+				// Creating platform
+				Expect(Kamel("install", "-n", ns2, "--skip-operator-setup", "--olm=false").Execute()).To(Succeed())
 
-			Expect(Kamel("run", "-n", ns2, "files/Java.java").Execute()).To(Succeed())
-			Eventually(IntegrationPodPhase(ns2, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
-			Eventually(IntegrationLogs(ns2, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
-			Expect(Kamel("delete", "--all", "-n", ns2).Execute()).To(Succeed())
-			Expect(Kits(ns2)()).Should(HaveLen(1))
-			Expect(Kits(ns)()).Should(HaveLen(0))
+				Expect(Kamel("run", "-n", ns2, "files/Java.java").Execute()).To(Succeed())
+				Eventually(IntegrationPodPhase(ns2, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
+				Eventually(IntegrationLogs(ns2, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
+				Expect(Kamel("delete", "--all", "-n", ns2).Execute()).To(Succeed())
+				Expect(Kits(ns2)()).Should(HaveLen(1))
+				Expect(Kits(ns)()).Should(HaveLen(0))
 
-			Expect(ConfigMap(ns2, platform.OperatorLockName)()).To(BeNil(), "No locking configmap expected")
+				Expect(ConfigMap(ns2, platform.OperatorLockName)()).To(BeNil(), "No locking configmap expected")
+			})
 		})
 
-		// NS3: namespace with its own operator
-		WithNewTestNamespace(t, func(ns3 string) {
-			Expect(Kamel("install", "-n", ns3, "--olm=false").Execute()).To(Succeed())
+		t.Run("Global test on namespace with its own operator", func(t *testing.T) {
+			WithNewTestNamespace(t, func(ns3 string) {
+				Expect(Kamel("install", "-n", ns3, "--olm=false").Execute()).To(Succeed())
 
-			Expect(Kamel("run", "-n", ns3, "files/Java.java").Execute()).To(Succeed())
-			Eventually(IntegrationPodPhase(ns3, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
-			Eventually(IntegrationLogs(ns3, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
-			Expect(Kamel("delete", "--all", "-n", ns3).Execute()).To(Succeed())
+				Expect(Kamel("run", "-n", ns3, "files/Java.java").Execute()).To(Succeed())
+				Eventually(IntegrationPodPhase(ns3, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
+				Eventually(IntegrationLogs(ns3, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
+				Expect(Kamel("delete", "--all", "-n", ns3).Execute()).To(Succeed())
 
-			Expect(ConfigMap(ns3, platform.OperatorLockName)()).ShouldNot(BeNil(),
-				"OperatorSDK is expected to use configmaps for locking: if this changes (e.g. using Leases) we should update our guard logic",
-			)
+				Expect(ConfigMap(ns3, platform.OperatorLockName)()).ShouldNot(BeNil(),
+					"OperatorSDK is expected to use configmaps for locking: if this changes (e.g. using Leases) we should update our guard logic",
+				)
+			})
 		})
 
-		// NS4: namespace without operator or platform
-		WithNewTestNamespace(t, func(ns4 string) {
-			Expect(Kamel("run", "-n", ns4, "files/Java.java").Execute()).To(Succeed())
-			Eventually(IntegrationPodPhase(ns4, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
-			Eventually(IntegrationLogs(ns4, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
-			Expect(Kamel("delete", "--all", "-n", ns4).Execute()).To(Succeed())
-			Expect(Kits(ns4)()).Should(HaveLen(0))
-			Expect(Kits(ns)()).Should(HaveLen(1)) // Kit built globally
+		t.Run("Global test on namespace without platform", func(t *testing.T) {
+			WithNewTestNamespace(t, func(ns4 string) {
+				Expect(Kamel("run", "-n", ns4, "files/Java.java").Execute()).To(Succeed())
+				Eventually(IntegrationPodPhase(ns4, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
+				Eventually(IntegrationLogs(ns4, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
+				Expect(Kamel("delete", "--all", "-n", ns4).Execute()).To(Succeed())
+				Expect(Kits(ns4)()).Should(HaveLen(0))
+				Expect(Kits(ns)()).Should(HaveLen(1)) // Kit built globally
 
-			Expect(ConfigMap(ns4, platform.OperatorLockName)()).To(BeNil(), "No locking configmap expected")
+				Expect(ConfigMap(ns4, platform.OperatorLockName)()).To(BeNil(), "No locking configmap expected")
+			})
 		})
 
-		// NS5: second namespace without operator or platform, using an external kit
-		WithNewTestNamespace(t, func(ns5 string) {
-			Expect(Kamel("run", "-n", ns5, "files/Java.java").Execute()).To(Succeed())
-			Eventually(IntegrationPodPhase(ns5, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
-			Eventually(IntegrationLogs(ns5, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
-			Expect(Kamel("delete", "--all", "-n", ns5).Execute()).To(Succeed())
-			Expect(Kits(ns5)()).Should(HaveLen(0))
-			globalKits := Kits(ns)()
-			Expect(globalKits).Should(HaveLen(1)) // Reusing the same global kit
-			if len(globalKits) == 1 {
-				kit := globalKits[0]
-				// external kit mirroring the global one
-				externalKit := camelv1.IntegrationKit{
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: ns5,
-						Name:      "external",
-						Labels: map[string]string{
-							"camel.apache.org/kit.type": camelv1.IntegrationKitTypeExternal,
-						},
-					},
-					Spec: camelv1.IntegrationKitSpec{
-						Image: kit.Status.Image,
-					},
-				}
-				Expect(TestClient().Create(TestContext, &externalKit)).Should(BeNil())
-
-				Expect(Kamel("run", "-n", ns5, "files/Java.java", "--name", "ext", "--kit", "external").Execute()).To(Succeed())
-				Eventually(IntegrationPodPhase(ns5, "ext"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
-				Eventually(IntegrationLogs(ns5, "ext"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
-				Expect(IntegrationKit(ns5, "ext")()).Should(Equal("external"))
+		t.Run("Global test on namespace without platform with external kit", func(t *testing.T) {
+			WithNewTestNamespace(t, func(ns5 string) {
+				Expect(Kamel("run", "-n", ns5, "files/Java.java").Execute()).To(Succeed())
+				Eventually(IntegrationPodPhase(ns5, "java"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
+				Eventually(IntegrationLogs(ns5, "java"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
 				Expect(Kamel("delete", "--all", "-n", ns5).Execute()).To(Succeed())
-				Expect(Kits(ns5)()).Should(HaveLen(1)) // the external one
-				Expect(Kits(ns)()).Should(HaveLen(1))  // the global one
-			}
+				Expect(Kits(ns5)()).Should(HaveLen(0))
+				globalKits := Kits(ns)()
+				Expect(globalKits).Should(HaveLen(1)) // Reusing the same global kit
+				if len(globalKits) == 1 {
+					kit := globalKits[0]
+					// external kit mirroring the global one
+					externalKit := camelv1.IntegrationKit{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: ns5,
+							Name:      "external",
+							Labels: map[string]string{
+								"camel.apache.org/kit.type": camelv1.IntegrationKitTypeExternal,
+							},
+						},
+						Spec: camelv1.IntegrationKitSpec{
+							Image: kit.Status.Image,
+						},
+					}
+					Expect(TestClient().Create(TestContext, &externalKit)).Should(BeNil())
 
-			Expect(ConfigMap(ns5, platform.OperatorLockName)()).To(BeNil(), "No locking configmap expected")
+					Expect(Kamel("run", "-n", ns5, "files/Java.java", "--name", "ext", "--kit", "external").Execute()).To(Succeed())
+					Eventually(IntegrationPodPhase(ns5, "ext"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
+					Eventually(IntegrationLogs(ns5, "ext"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
+					Expect(IntegrationKit(ns5, "ext")()).Should(Equal("external"))
+					Expect(Kamel("delete", "--all", "-n", ns5).Execute()).To(Succeed())
+					Expect(Kits(ns5)()).Should(HaveLen(1)) // the external one
+					Expect(Kits(ns)()).Should(HaveLen(1))  // the global one
+				}
+
+				Expect(ConfigMap(ns5, platform.OperatorLockName)()).To(BeNil(), "No locking configmap expected")
+			})
 		})
 
 		Expect(Kamel("uninstall", "-n", ns, "--skip-crd", "--skip-cluster-roles").Execute()).To(Succeed())
