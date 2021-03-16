f7ad34d9d6b8ad5f41f0be56f43171db8c9a8537
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2021-03-16 16:17:56 +0100

    feat(kamelets): error handling kamelets binding support
    * Added the capability to include an `errorHandler` support that will be converted as an `error-handler` in the generated integration.
    * Fixed the inspector_yaml.go to parse the integration `error-handler` and load declared Kamelet source

--- pkg/util/source/types.go
+++ pkg/util/source/types.go
@@ -25,6 +25,8 @@ type Metadata struct {
 	FromURIs []string
 	// All end URIs of defined routes
 	ToURIs []string
+	// All error handlers URIs of defined routes
+	ErrorHandlerURIs []string
 	// All inferred dependencies required to run the integration
 	Dependencies *strset.Set
 	// ExposesHTTPServices indicates if a route defined by the source is exposed
