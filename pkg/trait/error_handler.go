1c8f8b50c2156708a7480af9cb6142c75c0ea982
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2021-04-20 16:14:41 +0200

    feat(trait): error handler trait
    * KameletBinding can specify the error handler type
    * Introduced a trait to parse and create an error handler source type as expected by runtime.

---
+++ pkg/trait/error_handler.go
@@ -0,0 +1,162 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package trait
+
+import (
+	"fmt"
+	"strings"
+
+	v1 "github.com/apache/camel-k/pkg/apis/camel/v1"
+	"github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
+)
+
+// The error handler is a platform trait used to inject Error Handler source into the integration runtime.
+//
+// +camel-k:trait=error-handler
+type errorHandlerTrait struct {
+	BaseTrait `property:",squash"`
+	// Automatically inject all referenced Kamelets and their default configuration (enabled by default)
+	Auto *bool `property:"auto"`
+
+	// TODO move into a struct
+	ErrorHandlerURI  string
+	ErrorHandlerType string
+}
+
+func newErrorHandlerTrait() Trait {
+	return &errorHandlerTrait{
+		BaseTrait: NewBaseTrait("errorHandler", 500),
+	}
+}
+
+// IsPlatformTrait overrides base class method
+func (t *errorHandlerTrait) IsPlatformTrait() bool {
+	return true
+}
+
+func (t *errorHandlerTrait) Configure(e *Environment) (bool, error) {
+	if t.Enabled != nil && !*t.Enabled {
+		return false, nil
+	}
+
+	if !e.IntegrationInPhase(v1.IntegrationPhaseInitialization, v1.IntegrationPhaseDeploying, v1.IntegrationPhaseRunning) {
+		return false, nil
+	}
+
+	if t.Auto == nil || *t.Auto {
+		if t.ErrorHandlerType == "" {
+			t.ErrorHandlerType = maybeErrorHandler(e.Integration.Configurations())
+			if t.ErrorHandlerType != "" && v1alpha1.ErrorHandlerType(t.ErrorHandlerType) == v1alpha1.ErrorHandlerTypeDeadLetterChannel {
+				t.ErrorHandlerURI = maybeKameletAsDefaultErrorHandler(e.Integration.Configurations())
+			}
+		}
+	}
+
+	return t.ErrorHandlerType != "", nil
+}
+
+func maybeErrorHandler(properties []v1.ConfigurationSpec) string {
+	for _, property := range properties {
+		if strings.HasPrefix(property.Value, "camel.k.default-error-handler.type=") {
+			split := strings.Split(property.Value, "=")
+			if len(split) > 0 {
+				return split[1]
+			}
+		}
+	}
+
+	return ""
+}
+
+func maybeKameletAsDefaultErrorHandler(properties []v1.ConfigurationSpec) string {
+	for _, property := range properties {
+		if strings.HasPrefix(property.Value, "camel.k.default-error-handler.uri=") {
+			split := strings.Split(property.Value, "=")
+			if len(split) > 0 {
+				return split[1]
+			}
+		}
+	}
+
+	return ""
+}
+
+func (t *errorHandlerTrait) Apply(e *Environment) error {
+
+	if e.IntegrationInPhase(v1.IntegrationPhaseInitialization) {
+		if t.ErrorHandlerType != "" {
+			// Possible error handler
+			err := addErrorHandlerAsSource(e, t.ErrorHandlerURI, t.ErrorHandlerType)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func addErrorHandlerAsSource(e *Environment, errorHandlerURI string, errorHandlerType string) error {
+	errorHandlerStatement, err := parseErrorHandler(errorHandlerURI, errorHandlerType)
+	if err != nil {
+		return err
+	}
+
+	// TODO change to yaml flow when we fix https://issues.apache.org/jira/browse/CAMEL-16486
+	errorHandlerSource := v1.SourceSpec{
+		DataSpec: v1.DataSpec{
+			Name: "ErrorHandlerSource.java",
+			Content: fmt.Sprintf(`
+			import org.apache.camel.builder.RouteBuilder;
+			public class ErrorHandlerSource extends RouteBuilder {
+			@Override
+			public void configure() throws Exception {
+			  %s
+			  }
+			}
+			`, errorHandlerStatement),
+		},
+		Language: v1.LanguageJavaSource,
+		Type:     v1.SourceTypeErrorHandler,
+	}
+
+	replaced := false
+	for idx, existing := range e.Integration.Status.GeneratedSources {
+		if existing.Name == errorHandlerSource.Name {
+			replaced = true
+			e.Integration.Status.GeneratedSources[idx] = errorHandlerSource
+		}
+	}
+	if !replaced {
+		e.Integration.Status.GeneratedSources = append(e.Integration.Status.GeneratedSources, errorHandlerSource)
+	}
+
+	return nil
+}
+
+func parseErrorHandler(errHandlUri string, errHandlType string) (string, error) {
+	switch errHandlType {
+	case "no":
+		return `errorHandler(noErrorHandler());`, nil
+	case "default":
+		return `errorHandler(defaultErrorHandler());`, nil
+	case "dead-letter-channel":
+		return fmt.Sprintf(`errorHandler(deadLetterChannel("%v"));`, errHandlUri), nil
+	}
+
+	return "", fmt.Errorf("Cannot recognize any error handler of type %s", errHandlType)
+}
