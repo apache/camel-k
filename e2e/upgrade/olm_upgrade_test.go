6f64d3e1943ee3c33a8d47fab3137987c34f1543
Antonin Stefanutti <antonin@stefanutti.fr>
2021-05-11 14:41:17 +0200

    chore(e2e): Polish upgrade e2e tests

--- e2e/upgrade/olm_upgrade_test.go
+++ e2e/upgrade/olm_upgrade_test.go
@@ -23,19 +23,24 @@ package common
 
 import (
 	"fmt"
-	. "github.com/apache/camel-k/e2e/support"
-	. "github.com/onsi/gomega"
-	"github.com/operator-framework/api/pkg/lib/version"
-	"github.com/operator-framework/api/pkg/operators/v1alpha1"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
-	ctrl "sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
+
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	"github.com/operator-framework/api/pkg/lib/version"
+	olm "github.com/operator-framework/api/pkg/operators/v1alpha1"
+
+	. "github.com/apache/camel-k/e2e/support"
+	v1 "github.com/apache/camel-k/pkg/apis/camel/v1"
 )
 
-const CATALOG_SOURCE_NAME = "test-camel-k-source"
+const catalogSourceName = "test-camel-k-source"
 
 func TestOLMAutomaticUpgrade(t *testing.T) {
 	prevIIB := os.Getenv("CAMEL_K_PREV_IIB")
@@ -43,23 +48,23 @@ func TestOLMAutomaticUpgrade(t *testing.T) {
 	kamel := os.Getenv("RELEASED_KAMEL_BIN")
 
 	if prevIIB == "" || newIIB == "" {
-		t.Skip("OLM Upgrade test needs CAMEL_K_PREV_IIB and CAMEL_K_PREV_IIB ENV variables")
+		t.Skip("OLM Upgrade test requires the CAMEL_K_PREV_IIB and CAMEL_K_NEW_IIB environment variables")
 	}
 
 	WithNewTestNamespace(t, func(ns string) {
-		Expect(createCatalogSource(CATALOG_SOURCE_NAME, prevIIB, ns)).To(Succeed())
-		Eventually(CatalogSourcePhase(CATALOG_SOURCE_NAME, ns), TestTimeoutMedium).Should(Equal("READY"))
+		Expect(createOrUpdateCatalogSource(ns, catalogSourceName, prevIIB)).To(Succeed())
+		Eventually(catalogSourcePhase(ns, catalogSourceName), TestTimeoutMedium).Should(Equal("READY"))
 
-		//set KAMEL_BIN only for this test - don't override the ENV variable for all tests
+		// Set KAMEL_BIN only for this test - don't override the ENV variable for all tests
 		Expect(os.Setenv("KAMEL_BIN", kamel)).To(Succeed())
 
-		Expect(Kamel("install", "-n", ns, "--olm=true", "--olm-source", CATALOG_SOURCE_NAME, "--olm-source-namespace", ns).Execute()).To(Succeed())
+		Expect(Kamel("install", "-n", ns, "--olm=true", "--olm-source", catalogSourceName, "--olm-source-namespace", ns).Execute()).To(Succeed())
 
-		//find the only one Camel-K CSV
-		noAdditionalConditions := func(csv v1alpha1.ClusterServiceVersion) bool {
+		// Find the only one Camel-K CSV
+		noAdditionalConditions := func(csv olm.ClusterServiceVersion) bool {
 			return true
 		}
-		Eventually(CKClusterServiceVersionPhase(noAdditionalConditions, ns), TestTimeoutMedium).Should(Equal(v1alpha1.CSVPhaseSucceeded))
+		Eventually(clusterServiceVersionPhase(noAdditionalConditions, ns), TestTimeoutMedium).Should(Equal(olm.CSVPhaseSucceeded))
 
 		// Refresh the test client to account for the newly installed CRDs
 		SyncClient()
@@ -72,92 +77,92 @@ func TestOLMAutomaticUpgrade(t *testing.T) {
 		var prevIPVersionPrefix string
 		var newIPVersionPrefix string
 
-		prevCSVVersion = CKClusterServiceVersion(noAdditionalConditions, ns)().Spec.Version
+		prevCSVVersion = clusterServiceVersion(noAdditionalConditions, ns)().Spec.Version
 		prevIPVersionPrefix = fmt.Sprintf("%d.%d", prevCSVVersion.Version.Major, prevCSVVersion.Version.Minor)
 
-		Expect(OperatorPod(ns)).ToNot(BeNil())
+		// Check the operator pod is running
+		Eventually(OperatorPodPhase(ns), TestTimeoutMedium).Should(Equal(corev1.PodRunning))
 
-		Expect(Kamel("run", "-n", ns, "files/yaml.yaml").Execute()).To(Succeed())
-		Eventually(IntegrationPodPhase(ns, "yaml"), TestTimeoutMedium).Should(Equal(v1.PodRunning))
-
+		// Check the IntegrationPlatform has been reconciled
 		Eventually(PlatformVersion(ns)).Should(ContainSubstring(prevIPVersionPrefix))
-		Expect(IntegrationVersion(ns, "yaml")()).To(ContainSubstring(prevIPVersionPrefix))
+
+		name := "yaml"
+		Expect(Kamel("run", "-n", ns, "files/yaml.yaml").Execute()).To(Succeed())
+		Eventually(IntegrationPodPhase(ns, name), TestTimeoutMedium).Should(Equal(corev1.PodRunning))
+		// Check the Integration runs correctly
+		Eventually(IntegrationCondition(ns, name, v1.IntegrationConditionReady), TestTimeoutShort).Should(Equal(corev1.ConditionTrue))
+		// Check the Integration version matches that of the current operator
+		Expect(IntegrationVersion(ns, name)()).To(ContainSubstring(prevIPVersionPrefix))
 
 		t.Run("OLM upgrade", func(t *testing.T) {
+			// Trigger Camel K operator upgrade by updating the CatalogSource with the new index image
+			Expect(createOrUpdateCatalogSource(ns, catalogSourceName, newIIB)).To(Succeed())
 
-			//invoke OLM upgrade
-			Expect(createCatalogSource(CATALOG_SOURCE_NAME, newIIB, ns)).To(Succeed())
-
-			// previous CSV is REPLACING
-			Eventually(CKClusterServiceVersionPhase(func(csv v1alpha1.ClusterServiceVersion) bool {
+			// Check the previous CSV is being replaced
+			Eventually(clusterServiceVersionPhase(func(csv olm.ClusterServiceVersion) bool {
 				return csv.Spec.Version.Version.String() == prevCSVVersion.Version.String()
-			}, ns), TestTimeoutMedium).Should(Equal(v1alpha1.CSVPhaseReplacing))
+			}, ns), TestTimeoutMedium).Should(Equal(olm.CSVPhaseReplacing))
 
-			// the new version is installed
-			Eventually(CKClusterServiceVersionPhase(func(csv v1alpha1.ClusterServiceVersion) bool {
+			// The new CSV is installed
+			Eventually(clusterServiceVersionPhase(func(csv olm.ClusterServiceVersion) bool {
 				return csv.Spec.Version.Version.String() != prevCSVVersion.Version.String()
-			}, ns), TestTimeoutMedium).Should(Equal(v1alpha1.CSVPhaseSucceeded))
+			}, ns), TestTimeoutMedium).Should(Equal(olm.CSVPhaseSucceeded))
 
-			// the old version is gone
-			Eventually(CKClusterServiceVersion(func(csv v1alpha1.ClusterServiceVersion) bool {
+			// The old CSV is gone
+			Eventually(clusterServiceVersion(func(csv olm.ClusterServiceVersion) bool {
 				return csv.Spec.Version.Version.String() == prevCSVVersion.Version.String()
 			}, ns), TestTimeoutMedium).Should(BeNil())
 
-			newCSVVersion = CKClusterServiceVersion(noAdditionalConditions, ns)().Spec.Version
+			newCSVVersion = clusterServiceVersion(noAdditionalConditions, ns)().Spec.Version
 			newIPVersionPrefix = fmt.Sprintf("%d.%d", newCSVVersion.Version.Major, newCSVVersion.Version.Minor)
 
 			Expect(prevCSVVersion.Version.String()).NotTo(Equal(newCSVVersion.Version.String()))
 
-			Eventually(OperatorPodPhase(ns)).Should(Equal(v1.PodRunning))
+			Eventually(OperatorPodPhase(ns), TestTimeoutMedium).Should(Equal(corev1.PodRunning))
 
+			// Check the IntegrationPlatform has been reconciled
 			Eventually(PlatformVersion(ns)).Should(ContainSubstring(newIPVersionPrefix))
 		})
 
 		t.Run("Integration upgrade", func(t *testing.T) {
-
 			// Clear the KAMEL_BIN environment variable so that the current version is used from now on
 			Expect(os.Setenv("KAMEL_BIN", "")).To(Succeed())
 
-			Expect(IntegrationVersion(ns, "yaml")()).To(ContainSubstring(prevIPVersionPrefix))
-			Expect(Kamel("rebuild", "yaml", "-n", ns).Execute()).To(Succeed())
+			// Check the Integration is still running the old version
+			Expect(IntegrationVersion(ns, name)()).To(ContainSubstring(prevIPVersionPrefix))
 
-			Eventually(IntegrationVersion(ns, "yaml"), TestTimeoutMedium).Should(ContainSubstring(newIPVersionPrefix))
-			Eventually(IntegrationPodPhase(ns, "yaml")).Should(Equal(v1.PodRunning))
+			// Rebuild the Integration
+			Expect(Kamel("rebuild", name, "-n", ns).Execute()).To(Succeed())
+
+			// Check the Integration runs correctly
+			Eventually(IntegrationPodPhase(ns, name)).Should(Equal(corev1.PodRunning))
+			// Check the Integration has upgraded
+			Eventually(IntegrationVersion(ns, name), TestTimeoutMedium).Should(ContainSubstring(newIPVersionPrefix))
 
 			// Clean up
 			Expect(Kamel("delete", "--all", "-n", ns).Execute()).To(Succeed())
 			Expect(Kamel("uninstall", "-n", ns).Execute()).To(Succeed())
 		})
-
 	})
 }
 
-func createCatalogSource(name string, image string, ns string) error {
-	catalogSource := v1alpha1.CatalogSource{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "CatalogSource",
-			APIVersion: v1alpha1.SchemeGroupVersion.String(),
-		},
+func createOrUpdateCatalogSource(ns, name, image string) error {
+	catalogSource := &olm.CatalogSource{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: ns,
 			Name:      name,
 		},
 	}
-	key := ctrl.ObjectKey{
-		Namespace: ns,
-		Name:      name,
-	}
-	if err := TestClient().Get(TestContext, key, &catalogSource); errors.IsNotFound(err) {
-		catalogSource.Spec = v1alpha1.CatalogSourceSpec{
+
+	_, err := ctrlutil.CreateOrUpdate(TestContext, TestClient(), catalogSource, func() error {
+		catalogSource.Spec = olm.CatalogSourceSpec{
 			Image:       image,
 			SourceType:  "grpc",
 			DisplayName: "OLM upgrade test Catalog",
 			Publisher:   "grpc",
 		}
-		return TestClient().Create(TestContext, &catalogSource)
-	} else {
-		catalogSource.Spec.Image = image
-		return TestClient().Update(TestContext, &catalogSource)
-	}
+		return nil
+	})
 
+	return err
 }
