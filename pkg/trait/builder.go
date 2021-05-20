e20294fe6eb84bb2c062b274b79a92d1e58eb43d
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2021-05-20 16:19:00 +0200

    refactor(trait): property names and error condition

--- pkg/trait/builder.go
+++ pkg/trait/builder.go
@@ -25,7 +25,10 @@ import (
 	v1 "github.com/apache/camel-k/pkg/apis/camel/v1"
 	"github.com/apache/camel-k/pkg/builder"
 	corev1 "k8s.io/api/core/v1"
+<<<<<<< HEAD
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+=======
+>>>>>>> refactor(trait): property names and error condition
 )
 
 // The builder trait is internally used to determine the best strategy to
@@ -70,9 +73,12 @@ func (t *builderTrait) Apply(e *Environment) error {
 		e.IntegrationKit.Status.Phase = v1.IntegrationKitPhaseError
 		e.IntegrationKit.Status.SetCondition("IntegrationKitPropertiesFormatValid", corev1.ConditionFalse,
 			"IntegrationKitPropertiesFormatValid", fmt.Sprintf("One or more properties where not formatted as expected: %s", err.Error()))
+<<<<<<< HEAD
 		if _, err := e.Client.CamelV1().IntegrationKits(e.IntegrationKit.Namespace).UpdateStatus(e.C, e.IntegrationKit, metav1.UpdateOptions{}); err != nil {
 			return err
 		}
+=======
+>>>>>>> refactor(trait): property names and error condition
 		return nil
 	}
 
