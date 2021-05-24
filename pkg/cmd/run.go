552a51b71ccb13cc6675ef17b849305ff7ec5196
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2021-05-24 16:43:45 +0200

    feat(e2e): runtime and build time property files test
    Ref #2003

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -582,7 +582,6 @@ func (o *runCmdOptions) updateIntegrationCode(c client.Client, sources []string,
 	}
 	// convert each build configuration to a builder trait property
 	for _, item := range o.BuildProperties {
-<<<<<<< HEAD
 		props, err := extractProperties(item)
 		if err != nil {
 			return nil, err
@@ -595,9 +594,6 @@ func (o *runCmdOptions) updateIntegrationCode(c client.Client, sources []string,
 				return nil, err
 			}
 		}
-=======
-		o.Traits = append(o.Traits, fmt.Sprintf("builder.properties=%s", item))
->>>>>>> refactor(trait): property names and error condition
 	}
 	for _, item := range o.LoggingLevels {
 		integration.Spec.AddConfiguration("property", "logging.level."+item)
