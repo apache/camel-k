1019b9dc2904f85c69f39a8fd5157e3cca32c0aa
nicolaferraro <ni.ferraro@gmail.com>
2021-07-02 12:15:57 +0200

    chore(release): prepare 1.5.0 release

--- pkg/util/defaults/defaults.go
+++ pkg/util/defaults/defaults.go
@@ -26,7 +26,7 @@ const (
 	Version = "1.5.0-SNAPSHOT"
 
 	// DefaultRuntimeVersion --
-	DefaultRuntimeVersion = "1.8.0-SNAPSHOT"
+	DefaultRuntimeVersion = "1.8.0"
 
 	// BuildahVersion --
 	BuildahVersion = "1.14.0"
