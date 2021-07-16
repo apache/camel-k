32d380833bac39b07904ed18b5942c828d8a9a51
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2021-07-16 21:51:35 +0100

    Fix - failing upgrade e2e test
    *  clusterserviceversion.yaml
     * Snapshot suffix violates semver restrictions of operator-sdk
    
    * Makefile
     * Restore overriding of VERSION & IMAGE_NAME for use with kustomize install
     * Restricts the add_license.sh to stop the patch created by the e2e test
       receiving a licence header and failing to be applied correctly
    
    * next_shapshot.sh
     * updates script to replace call to get_version.sh which no longer exists

--- script/next_snapshot.sh
+++ script/next_snapshot.sh
@@ -20,7 +20,7 @@ set -e
 location=$(dirname $0)
 global_version_file=$location/../version/version.go
 
-version=$($location/get_version.sh)
+version=$(cd $location && make get-version)
 version_num=$(echo $version | sed -E "s/([0-9.]*)-SNAPSHOT/\1/g")
 next_version_num=$(echo $version_num | awk 'BEGIN { FS = "." } ; {print $1"."$2"."++$3}')
 next_version="$next_version_num-SNAPSHOT"
