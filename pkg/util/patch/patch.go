3707e3d6095d7e3ff85070bf5caa5b774f1d8cb1
Antonin Stefanutti <antonin@stefanutti.fr>
2021-10-13 17:05:34 +0200

    chore: Bypass cache round-trips to reconcile integration status

--- pkg/util/patch/patch.go
+++ pkg/util/patch/patch.go
@@ -25,11 +25,9 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/json"
-
-	ctrl "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func PositiveMergePatch(source runtime.Object, target runtime.Object) ([]byte, error) {
+func PositiveMergePatch(source interface{}, target interface{}) ([]byte, error) {
 	sourceJSON, err := json.Marshal(source)
 	if err != nil {
 		return nil, err
@@ -65,7 +63,7 @@ func PositiveMergePatch(source runtime.Object, target runtime.Object) ([]byte, e
 	return json.Marshal(positivePatch)
 }
 
-func PositiveApplyPatch(source runtime.Object) (ctrl.Object, error) {
+func PositiveApplyPatch(source runtime.Object) (*unstructured.Unstructured, error) {
 	sourceJSON, err := json.Marshal(source)
 	if err != nil {
 		return nil, err
