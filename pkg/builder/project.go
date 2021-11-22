b44621d781057a3e5071b74d6e80f783af2f564a
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-11-22 15:25:47 +0100

    fix(gosec): expect directory permissions to be 0750 or less (G301)

--- pkg/builder/project.go
+++ pkg/builder/project.go
@@ -65,7 +65,7 @@ func cleanUpBuildDir(ctx *builderContext) error {
 		return err
 	}
 
-	return os.MkdirAll(ctx.Build.BuildDir, 0o777)
+	return os.MkdirAll(ctx.Build.BuildDir, 0o700)
 }
 
 func generateJavaKeystore(ctx *builderContext) error {
