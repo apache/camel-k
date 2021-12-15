fc5f70689660c68c9850257ea41ecfda2861d6ef
nicolaferraro <ni.ferraro@gmail.com>
2021-12-15 10:36:40 +0100

    Fix #1107: initial trait

---
+++ addons/keda/duck/v1alpha1/duck_types.go
@@ -0,0 +1,108 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:onlyVerbs=get,list,watch
+// +genclient:noStatus
+// +kubebuilder:object:root=true
+
+// ScaledObject is a specification for a ScaledObject resource
+type ScaledObject struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ScaledObjectSpec `json:"spec"`
+}
+
+// ScaledObjectSpec is the spec for a ScaledObject resource
+type ScaledObjectSpec struct {
+	ScaleTargetRef *v1.ObjectReference `json:"scaleTargetRef"`
+
+	Triggers []ScaleTriggers `json:"triggers"`
+}
+
+// ScaleTriggers reference the scaler that will be used
+type ScaleTriggers struct {
+	Type string `json:"type"`
+	// +optional
+	Name     string            `json:"name,omitempty"`
+	Metadata map[string]string `json:"metadata"`
+	// +optional
+	AuthenticationRef *ScaledObjectAuthRef `json:"authenticationRef,omitempty"`
+	// +optional
+	FallbackReplicas *int32 `json:"fallback,omitempty"`
+}
+
+// ScaledObjectAuthRef points to the TriggerAuthentication or ClusterTriggerAuthentication object that
+// is used to authenticate the scaler with the environment
+type ScaledObjectAuthRef struct {
+	Name string `json:"name"`
+	// Kind of the resource being referred to. Defaults to TriggerAuthentication.
+	// +optional
+	Kind string `json:"kind,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ScaledObjectList contains a list of ScaledObject.
+type ScaledObjectList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ScaledObject `json:"items"`
+}
+
+// +genclient
+// +genclient:onlyVerbs=get,list,watch
+// +genclient:noStatus
+// +kubebuilder:object:root=true
+
+// TriggerAuthentication defines how a trigger can authenticate
+type TriggerAuthentication struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec TriggerAuthenticationSpec `json:"spec"`
+}
+
+// TriggerAuthenticationSpec defines the various ways to authenticate
+type TriggerAuthenticationSpec struct {
+	// +optional
+	SecretTargetRef []AuthSecretTargetRef `json:"secretTargetRef,omitempty"`
+}
+
+// AuthSecretTargetRef is used to authenticate using a reference to a secret
+type AuthSecretTargetRef struct {
+	Parameter string `json:"parameter"`
+	Name      string `json:"name"`
+	Key       string `json:"key"`
+}
+
+// +kubebuilder:object:root=true
+
+// TriggerAuthenticationList contains a list of TriggerAuthentication
+type TriggerAuthenticationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []TriggerAuthentication `json:"items"`
+}
--- addons/keda/keda.go
+++ addons/keda/keda.go
@@ -18,7 +18,16 @@ limitations under the License.
 package keda
 
 import (
+	"strings"
+
+	kedav1alpha1 "github.com/apache/camel-k/addons/keda/duck/v1alpha1"
+	camelv1 "github.com/apache/camel-k/pkg/apis/camel/v1"
+	"github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
+	camelv1alpha1 "github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
 	"github.com/apache/camel-k/pkg/trait"
+	scase "github.com/stoewer/go-strcase"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // The Keda trait can be used for automatic integration with Keda autoscalers.
@@ -30,7 +39,16 @@ type kedaTrait struct {
 	trait.BaseTrait `property:",squash"`
 	// Enables automatic configuration of the trait.
 	Auto *bool `property:"auto" json:"auto,omitempty"`
-	// Metadata
+	// Convert metadata properties to camelCase (needed because trait properties use kebab-case). Enabled by default.
+	CamelCaseConversion *bool `property:"camel-case-conversion" json:"camelCaseConversion,omitempty"`
+	// Set the spec->replicas field on the top level controller to an explicit value if missing, to allow Keda to recognize it as a scalable resource
+	HackControllerReplicas *bool `property:"hack-controller-replicas" json:"hackControllerReplicas,omitempty"`
+	// Triggers
+	Triggers []kedaTrigger `property:"triggers" json:"triggers,omitempty"`
+}
+
+type kedaTrigger struct {
+	Type     string            `property:"type" json:"type,omitempty"`
 	Metadata map[string]string `property:"metadata" json:"metadata,omitempty"`
 }
 
@@ -42,10 +60,106 @@ func NewKedaTrait() trait.Trait {
 }
 
 func (t *kedaTrait) Configure(e *trait.Environment) (bool, error) {
+	if t.Enabled == nil || !*t.Enabled {
+		return false, nil
+	}
 
-	return false, nil
+	if !e.IntegrationInPhase(camelv1.IntegrationPhaseInitialization) && !e.IntegrationInRunningPhases() {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 func (t *kedaTrait) Apply(e *trait.Environment) error {
+	if e.IntegrationInPhase(camelv1.IntegrationPhaseInitialization) {
+		if t.HackControllerReplicas == nil || *t.HackControllerReplicas {
+			if err := t.hackControllerReplicas(e); err != nil {
+				return err
+			}
+		}
+	} else if e.IntegrationInRunningPhases() {
+		if so, err := t.getScaledObject(e); err != nil {
+			return err
+		} else if so != nil {
+			e.Resources.Add(so)
+		}
+	}
+
 	return nil
+}
+
+func (t *kedaTrait) getScaledObject(e *trait.Environment) (*kedav1alpha1.ScaledObject, error) {
+	if len(t.Triggers) == 0 {
+		return nil, nil
+	}
+	obj := kedav1alpha1.NewScaledObject(e.Integration.Namespace, e.Integration.Name)
+	obj.Spec.ScaleTargetRef = t.getTopControllerReference(e)
+
+	for _, trigger := range t.Triggers {
+		meta := make(map[string]string)
+		for k, v := range trigger.Metadata {
+			kk := k
+			if t.CamelCaseConversion == nil || *t.CamelCaseConversion {
+				kk = scase.LowerCamelCase(k)
+			}
+			meta[kk] = v
+		}
+		st := kedav1alpha1.ScaleTriggers{
+			Type:     trigger.Type,
+			Metadata: meta,
+		}
+		obj.Spec.Triggers = append(obj.Spec.Triggers, st)
+	}
+
+	return &obj, nil
+}
+
+func (t *kedaTrait) hackControllerReplicas(e *trait.Environment) error {
+	ctrlRef := t.getTopControllerReference(e)
+
+	if ctrlRef.Kind == camelv1alpha1.KameletBindingKind {
+		// Update the KameletBinding directly (do not add it to env resources, it's the integration parent)
+		key := client.ObjectKey{
+			Namespace: e.Integration.Namespace,
+			Name:      ctrlRef.Name,
+		}
+		klb := camelv1alpha1.KameletBinding{}
+		if err := e.Client.Get(e.Ctx, key, &klb); err != nil {
+			return err
+		}
+		if klb.Spec.Replicas == nil {
+			one := int32(1)
+			klb.Spec.Replicas = &one
+			if err := e.Client.Update(e.Ctx, &klb); err != nil {
+				return err
+			}
+		}
+	} else {
+		if e.Integration.Spec.Replicas == nil {
+			one := int32(1)
+			e.Integration.Spec.Replicas = &one
+			if err := e.Client.Update(e.Ctx, e.Integration); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (t *kedaTrait) getTopControllerReference(e *trait.Environment) *v1.ObjectReference {
+	for _, o := range e.Integration.OwnerReferences {
+		if o.Kind == v1alpha1.KameletBindingKind && strings.HasPrefix(o.APIVersion, v1alpha1.SchemeGroupVersion.Group) {
+			return &v1.ObjectReference{
+				APIVersion: o.APIVersion,
+				Kind:       o.Kind,
+				Name:       o.Name,
+			}
+		}
+	}
+	return &v1.ObjectReference{
+		APIVersion: e.Integration.APIVersion,
+		Kind:       e.Integration.Kind,
+		Name:       e.Integration.Name,
+	}
 }
