7b49bbe74ccb677aa7c343bff8c4ab986689fcf4
Claudio Miranda <claudio@claudius.com.br>
2022-05-02 20:39:12 -0300

    package_maven_artifacts: run unzip in silent mode

--- script/package_maven_artifacts.sh
+++ script/package_maven_artifacts.sh
@@ -35,7 +35,7 @@ if [ -z "$2" ]; then
 
     # Take the dependencies officially released
     wget https://repo1.maven.org/maven2/org/apache/camel/k/apache-camel-k-runtime/$1/apache-camel-k-runtime-$1-source-release.zip -O $PWD/build/apache-camel-k-runtime-$1-source-release.zip
-    unzip -o $PWD/build/apache-camel-k-runtime-$1-source-release.zip -d $PWD/build
+    unzip -q -o $PWD/build/apache-camel-k-runtime-$1-source-release.zip -d $PWD/build
     mvn -q -f $PWD/build/apache-camel-k-runtime-$1/pom.xml \
         dependency:copy-dependencies \
         -DincludeScope=runtime \
