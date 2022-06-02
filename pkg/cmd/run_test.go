417b7b873f4aea59488cddfcc9ddf2f548248194
Christoph Deppisch <cdeppisch@redhat.com>
2022-06-02 16:53:53 +0200

    Fix #2177: Ensure valid operator id when running integrations
    - Make sure to use a known operator id when running an integration
    - Check performs a cluster wide search for integration platforms and verifies that the platform name exists
    - Users may use --force option to skip the check

--- pkg/cmd/run_test.go
+++ pkg/cmd/run_test.go
@@ -27,6 +27,7 @@ import (
 
 	v1 "github.com/apache/camel-k/pkg/apis/camel/v1"
 	traitv1 "github.com/apache/camel-k/pkg/apis/camel/v1/trait"
+	"github.com/apache/camel-k/pkg/platform"
 	"github.com/apache/camel-k/pkg/trait"
 	"github.com/apache/camel-k/pkg/util/test"
 
@@ -55,7 +56,10 @@ func initializeRunCmdOptions(t *testing.T) (*runCmdOptions, *cobra.Command, Root
 func initializeRunCmdOptionsWithOutput(t *testing.T) (*runCmdOptions, *cobra.Command, RootCmdOptions) {
 	t.Helper()
 
-	options, rootCmd := kamelTestPreAddCommandInit()
+	defaultIntegrationPlatform := v1.NewIntegrationPlatform("default", platform.DefaultPlatformName)
+	fakeClient, _ := test.NewFakeClient(&defaultIntegrationPlatform)
+
+	options, rootCmd := kamelTestPreAddCommandInitWithClient(fakeClient)
 	runCmdOptions := addTestRunCmdWithOutput(*options, rootCmd)
 	kamelTestPostAddCommandInit(t, rootCmd)
 
