94707e1a5280fe1fe2c8bf3c28a460f49bedbc82
Andrea Cosentino <ancosen@gmail.com>
2022-08-23 06:50:10 +0200

    Point to camel-k-runtime 1.14.0 as default

--- pkg/util/defaults/defaults.go
+++ pkg/util/defaults/defaults.go
@@ -26,7 +26,7 @@ const (
 	Version = "1.10.0-SNAPSHOT"
 
 	// DefaultRuntimeVersion --
-	DefaultRuntimeVersion = "1.13.0"
+	DefaultRuntimeVersion = "1.14.0"
 
 	// BuildahVersion --
 	BuildahVersion = "1.23.3"
