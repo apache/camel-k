ee2ba9430b7357274bb01f0d99e93c380464434f
Andrea Cosentino <ancosen@gmail.com>
2022-09-28 10:48:19 +0200

    Refactor vault folder by creating subfolders for different cloud providers

--- script/gen_doc.sh
+++ script/gen_doc.sh
@@ -29,5 +29,6 @@ go run ./cmd/util/doc-gen \
   --input-dirs github.com/apache/camel-k/addons/keda \
   --input-dirs github.com/apache/camel-k/addons/master \
   --input-dirs github.com/apache/camel-k/addons/threescale \
-  --input-dirs github.com/apache/camel-k/addons/tracing
+  --input-dirs github.com/apache/camel-k/addons/tracing \
+  --input-dirs github.com/apache/camel-k/addons/vault/aws 
 echo "Generating traits documentation... done!"
