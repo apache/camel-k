ac485b3adbbd3f421c06b7e7e4c86852e4eaec55
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2022-10-21 12:03:59 +0200

    feat(ci): latest runtime snapshot available

--- script/get_catalog.sh
+++ script/get_catalog.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 location=$(dirname $0)
 rootdir=$location/../
 
@@ -23,30 +25,27 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 runtime_version="$1"
-if [ -z $2 ]; then
-  mvn -q dependency:copy -Dartifact="org.apache.camel.k:camel-k-catalog:$runtime_version:yaml:catalog" -DoutputDirectory=${rootdir}/resources/
-  if [ -f "${rootdir}/resources/camel-k-catalog-${runtime_version}-catalog.yaml" ]; then
-      mv ${rootdir}/resources/camel-k-catalog-"${runtime_version}"-catalog.yaml ${rootdir}/resources/camel-catalog-"${runtime_version}".yaml
-  elif [[ $runtime_version == *"SNAPSHOT" ]]; then
-      # when using SNAPSHOT versions and a snapshot repository the downloaded artifact has a timestamp in place of the SNAPSHOT word
-      # we should replace this timestamp with the SNAPSHOT word
-      only_version=$(echo $runtime_version|sed 's/-SNAPSHOT//g')
-      cat_file=$(ls -lrt ${rootdir}/resources/camel-k-catalog-"${only_version}"*-catalog.yaml|tail -1|sed 's/.*resources\///g')
-      mv ${rootdir}/resources/$cat_file ${rootdir}/resources/camel-catalog-"${runtime_version}".yaml
-  fi
-else
-  # TODO: fix this workaround to use the above mvn statement with the staging repository as well
-  echo "INFO: extracting a catalog from staging repository $2"
-  wget -q $2/org/apache/camel/k/camel-k-catalog/$runtime_version/camel-k-catalog-$runtime_version-catalog.yaml -O ${rootdir}/resources/camel-catalog.yaml
 
-  if [ -s ${rootdir}/resources/camel-catalog.yaml ]; then
-    # the extracted catalog file is not empty
-    mv ${rootdir}/resources/camel-catalog.yaml ${rootdir}/resources/camel-catalog-$runtime_version.yaml
-  else
-    # the extracted catalog file is empty - some error in staging repository
-    echo "WARNING: could not extract catalog from staging repository $2"
-    rm ${rootdir}/resources/camel-catalog.yaml
-  fi
+if [ ! -z $2 ]; then
+  # Change the settings to include the staging repo if it's not already there
+  echo "INFO: updating the settings staging repository"
+  sed -i "s;<url>https://repository\.apache\.org/content/repositories/orgapachecamel-.*</url>;<url>$2</url>;" $location/maven-settings.xml
 fi
 
+# Refresh catalog sets. We can clean any leftover as well.
+rm -f ${rootdir}/resources/camel-catalog-*
+
+mvn -q dependency:copy -Dartifact="org.apache.camel.k:camel-k-catalog:$runtime_version:yaml:catalog" \
+  -DoutputDirectory=${rootdir}/resources/ \
+  -s $location/maven-settings.xml \
+  -Papache-snapshots
+if [ -f "${rootdir}/resources/camel-k-catalog-${runtime_version}-catalog.yaml" ]; then
+    mv ${rootdir}/resources/camel-k-catalog-"${runtime_version}"-catalog.yaml ${rootdir}/resources/camel-catalog-"${runtime_version}".yaml
+elif [[ $runtime_version == *"SNAPSHOT" ]]; then
+    # when using SNAPSHOT versions and a snapshot repository the downloaded artifact has a timestamp in place of the SNAPSHOT word
+    # we should replace this timestamp with the SNAPSHOT word
+    only_version=$(echo $runtime_version|sed 's/-SNAPSHOT//g')
+    cat_file=$(ls -lrt ${rootdir}/resources/camel-k-catalog-"${only_version}"*-catalog.yaml|tail -1|sed 's/.*resources\///g')
+    mv ${rootdir}/resources/$cat_file ${rootdir}/resources/camel-catalog-"${runtime_version}".yaml
+fi
 
