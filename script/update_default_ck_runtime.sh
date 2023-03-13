5595b36fcea838e8676eee91c6e7520bc8a69f54
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2023-03-13 18:54:15 +0100

    feat(ci): scrape latest CK runtime available

---
+++ script/update_default_ck_runtime.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ---------------------------------------------------------------------------
+
+set -e
+
+location=$(dirname $0)
+
+git clone --depth 1 https://github.com/apache/camel-k-runtime.git /tmp/camel-k-runtime
+ck_runtime_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout -f /tmp/camel-k-runtime/pom.xml)
+echo "INFO: last Camel K runtime version set at $ck_runtime_version"
+sed -i "s/^RUNTIME_VERSION := .*$/RUNTIME_VERSION := $ck_runtime_version/" $location/Makefile
+rm -rf /tmp/camel-k-runtime/
