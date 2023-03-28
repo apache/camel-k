79cd89c34aaebe625398611acf1fb1110ad08602
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2023-03-28 15:59:04 +0200

    chore(api): add a deprecation notice

--- pkg/apis/camel/v1alpha1/error_handler_types.go
+++ pkg/apis/camel/v1alpha1/error_handler_types.go
@@ -15,6 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package v1alpha1 is deprecated: use the v1 API instead
+// Deprecated
 package v1alpha1
 
 const (
