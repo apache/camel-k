d7a6820f5a01c7d63d2b0e6cd667582ec1f2112e
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2023-05-30 11:18:17 +0200

    fix(core): Minimal configuration needed for S2I to work (root user on FS access for operator)

--- pkg/install/operator.go
+++ pkg/install/operator.go
@@ -205,6 +205,10 @@ func OperatorOrCollect(ctx context.Context, cmd *cobra.Command, c client.Client,
 					fmt.Sprintf("--health-port=%d", cfg.Health.Port))
 				d.Spec.Template.Spec.Containers[0].LivenessProbe.HTTPGet.Port = intstr.FromInt(int(cfg.Health.Port))
 			}
+			var ugfid int64 = 0
+			d.Spec.Template.Spec.SecurityContext = &corev1.PodSecurityContext{
+				FSGroup: &ugfid,
+			}
 		}
 
 		if cfg.Global {
