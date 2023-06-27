d61f7da7bbbb5d3f3326978514f5ad18213570a8
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2023-06-27 11:21:32 +0200

    fix(traits): set native build also when Kit native

--- pkg/trait/builder.go
+++ pkg/trait/builder.go
@@ -173,8 +173,14 @@ func (t *builderTrait) builderTask(e *Environment) (*v1.BuilderTask, error) {
 	}
 
 	if trait := e.Catalog.GetTrait(quarkusTraitID); trait != nil {
+		quarkus, ok := trait.(*quarkusTrait)
+		isNativeIntegration := quarkus.isNativeIntegration(e)
+		isNativeKit, err := quarkus.isNativeKit(e)
+		if err != nil {
+			return nil, err
+		}
 		// The builder trait must define certain resources requirements when we have a native build
-		if quarkus, ok := trait.(*quarkusTrait); ok && pointer.BoolDeref(quarkus.Enabled, true) && quarkus.isNativeIntegration(e) {
+		if ok && pointer.BoolDeref(quarkus.Enabled, true) && (isNativeIntegration || isNativeKit) {
 			// Force the build to run in a separate Pod
 			t.L.Info("This is a Quarkus native build: setting build configuration with build Pod strategy, 1 CPU core and 4 GiB memory. Make sure your cluster can handle it.")
 			t.Strategy = string(v1.BuildStrategyPod)
