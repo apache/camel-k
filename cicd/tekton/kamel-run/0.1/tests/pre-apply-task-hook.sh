15458b8e7e8f1fa2b698101ab478c3309a59c960
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2023-07-25 10:15:43 +0200

    chore(ci): bump Tekton task

--- cicd/tekton/kamel-run/0.1/tests/pre-apply-task-hook.sh
+++ cicd/tekton/kamel-run/0.1/tests/pre-apply-task-hook.sh
@@ -20,8 +20,8 @@
 add_sidecar_registry ${TMPF}
 
 # Install Camel K operator
-wget https://github.com/apache/camel-k/releases/download/v1.12.0/camel-k-client-1.12.0-linux-64bit.tar.gz
-tar -xvf camel-k-client-1.12.0-linux-64bit.tar.gz
+wget https://github.com/apache/camel-k/releases/download/v2.0.0/camel-k-client-2.0.0-linux-amd64.tar.gz
+tar -xvf camel-k-client-2.0.0-linux-amd64.tar.gz
 ./kamel install --registry localhost:5000 --registry-insecure --wait
 
 # Add git-clone
