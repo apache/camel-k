20edf376bd8992f6c9f8f2feea45e9f2ba5ade41
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2023-09-20 14:34:59 +0200

    feat(runtime): default version 3.2.0
    * Based on Camel Quarkus 3.2.0/Camel 4
    * Based on Kamelets 4.0.0

--- pkg/util/defaults/defaults.go
+++ pkg/util/defaults/defaults.go
@@ -26,7 +26,7 @@ const (
 	Version = "2.1.0-SNAPSHOT"
 
 	// DefaultRuntimeVersion --
-	DefaultRuntimeVersion = "2.16.0"
+	DefaultRuntimeVersion = "3.2.0"
 
 	// BuildahVersion --
 	BuildahVersion = "1.30.0"
