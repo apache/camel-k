77b029077d35d7e4d2881d0c67ca23f9eafac8af
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2023-10-02 16:52:06 +0200

    fix(ctrl): Change reconciliation of int in error
    * When the integration is in Error with the Kit condition in error, then if the integration kit referenced is still in error then finish the reconciliation process without any change to the integration.

--- pkg/trait/jvm.go
+++ pkg/trait/jvm.go
@@ -124,7 +124,7 @@ func (t *jvmTrait) Apply(e *Environment) error {
 
 	container := e.GetIntegrationContainer()
 	if container == nil {
-		return fmt.Errorf("unable to find integration container")
+		return fmt.Errorf("unable to find integration container: %s", e.Integration.Name)
 	}
 
 	// Build the container command
