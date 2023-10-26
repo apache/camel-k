df50bf123f47990d59680f2f59ec80de3929297f
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2023-10-26 12:33:20 +0200

    fix(ci): attempt

--- .github/actions/kamel-build-bundle/build-bundle-image.sh
+++ .github/actions/kamel-build-bundle/build-bundle-image.sh
@@ -110,7 +110,7 @@ export PUSH_BUNDLE_LOCAL_IMAGE=${REGISTRY_PUSH_HOST}/${IMAGE_NAMESPACE}/${BUNDLE
 export CUSTOM_IMAGE=${IMAGE_NAME}
 export CUSTOM_VERSION=${IMAGE_VERSION}
 
-export PREV_XY_CHANNEL="stable-$(make get-last-released-version | grep -Po '\d+\.\d+')"
+export PREV_XY_CHANNEL="stable-v2"
 echo "PREV_XY_CHANNEL=${PREV_XY_CHANNEL}" >> $GITHUB_ENV
 export NEW_XY_CHANNEL=stable-dev-$(make get-version | grep -Po "\d+\.\d+")
 echo "NEW_XY_CHANNEL=${NEW_XY_CHANNEL}" >> $GITHUB_ENV
