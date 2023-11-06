3196a90d22f26239e3ae98436e63784507b0db53
Claudio Miranda <claudio@claudius.com.br>
2023-11-06 06:46:31 -0300

    Fix SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder" (#4883)
    moved camel-k-maven-logging from camel-k-runtime to camel-k project
    to make the maven logging independent of the camel-k-runtime
    slf4j-api 1.7 originally from maven zip should be removed
    
    Fix https://github.com/apache/camel-k/issues/4882
    
    (cherry picked from commit 0c1c01d11b0c9ad8f9c7e36187999a1f9b521998)

--- script/maven_overlay.sh
+++ script/maven_overlay.sh
@@ -18,22 +18,8 @@
 location=$(dirname $0)
 rootdir=$(realpath ${location}/../)
 
-while getopts "d:s:" opt; do
-  case "${opt}" in
-    d)
-      local_runtime_dir="${OPTARG}"
-      ;;
-    s)
-      staging_repo="${OPTARG}"
-      ;;
-    *)
-      ;;
-  esac
-done
-shift $((OPTIND-1))
-
-if [ "$#" -lt 2 ]; then
-  echo "usage: $0 [-s <staging repository>] [-d <local Camel K runtime project directory>] <Camel K runtime version> <output directory>"
+if [ "$#" -lt 1 ]; then
+  echo "usage: $0 <output directory>"
   exit 1
 fi
 
@@ -42,39 +28,8 @@ if [ "$CI" = "true" ]; then
   options="--batch-mode"
 fi
 
-runtime_version=$1
-output_dir=$2
+output_dir=$1
 
-if [ ! -z $staging_repo ]; then
-  # Change the settings to include the staging repo if it's not already there
-  echo "INFO: updating the settings staging repository"
-  sed -i "s;<url>https://repository\.apache\.org/content/repositories/orgapachecamel-.*</url>;<url>$staging_repo</url>;" $location/maven-settings.xml
-fi
-
-if [ ! -z "$local_runtime_dir" ]; then
-    # Take the dependencies from a local development environment
-    camel_k_runtime_source_version=$(mvn -f $local_runtime_dir/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
-
-    if [ "$runtime_version" != "$camel_k_runtime_source_version" ]; then
-        echo "WARNING! You're building Camel K project using $runtime_version but trying to bundle dependencies from local Camel K runtime $camel_k_runtime_source_version"
-    fi
-
-    mvn -q \
-      $options \
-      -am \
-      -f $local_runtime_dir/support/camel-k-maven-logging/pom.xml \
-      install
-fi
-
-mvn -q \
-  $options \
-  dependency:copy \
-  -Dartifact=org.apache.camel.k:camel-k-maven-logging:${runtime_version}:zip \
-  -D mdep.useBaseVersion=true \
-  -DoutputDirectory=${rootdir}/${output_dir} \
-  -Papache \
-  -s $location/maven-settings.xml
-
-unzip -q -o ${rootdir}/${output_dir}/camel-k-maven-logging-${runtime_version}.zip -d ${rootdir}/${output_dir}
-rm -f camel-k-maven-logging-${runtime_version}.zip
-
+mvn $options -f java/maven-logging/pom.xml clean package
+cp java/maven-logging/target/maven-overlay/*.jar ${output_dir}
+cp java/maven-logging/src/main/resources/logback.xml ${output_dir}
