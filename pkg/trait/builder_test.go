77df16eecae1a23d6393f693fdc57b1c2ecdfa2f
Luis Sergio Carneiro <luis.carneiro@sensedia.com>
2023-12-06 09:42:35 -0300

    feat(#4958): adding unit tests

--- pkg/trait/builder_test.go
+++ pkg/trait/builder_test.go
@@ -449,3 +449,34 @@ func TestExistsTaskRequest(t *testing.T) {
 	assert.False(t, existsTaskRequest(tasks, "quarkus"))
 	assert.False(t, existsTaskRequest(tasks, "shouldfail"))
 }
+
+func TestBuilderWithNoNodeSelector(t *testing.T) {
+	env := createBuilderTestEnv(v1.IntegrationPlatformClusterKubernetes, v1.IntegrationPlatformBuildPublishStrategyJib, v1.BuildStrategyRoutine)
+	builderTrait := createNominalBuilderTraitTest()
+
+	active, condition, err := builderTrait.Configure(env)
+
+	assert.Nil(t, err)
+	assert.True(t, active)
+	assert.Nil(t, condition)
+
+	assert.Nil(t, builderTrait.NodeSelector)
+	assert.Nil(t, env.BuilderNodeSelector)
+}
+
+func TestBuilderWithNodeSelector(t *testing.T) {
+	env := createBuilderTestEnv(v1.IntegrationPlatformClusterKubernetes, v1.IntegrationPlatformBuildPublishStrategyJib, v1.BuildStrategyRoutine)
+	builderTrait := createNominalBuilderTraitTest()
+	builderTrait.NodeSelector = map[string]string{
+		"size": "large",
+	}
+
+	active, condition, err := builderTrait.Configure(env)
+
+	assert.Nil(t, err)
+	assert.True(t, active)
+	assert.Nil(t, condition)
+
+	assert.Equal(t, map[string]string{"size": "large"}, env.BuilderNodeSelector)
+	assert.Equal(t, map[string]string{"size": "large"}, builderTrait.NodeSelector)
+}
