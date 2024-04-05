025bf44bec623bbb66dc48bb0b6097448e298322
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2024-04-05 11:30:29 +0200

    chore(doc): trait doc update

--- pkg/apis/camel/v1/trait/camel.go
+++ pkg/apis/camel/v1/trait/camel.go
@@ -23,6 +23,8 @@ package trait
 type CamelTrait struct {
 	PlatformBaseTrait `property:",squash" json:",inline"`
 	// The camel-k-runtime version to use for the integration. It overrides the default version set in the Integration Platform.
+	// You can use a fixed version (for example "3.2.3") or a semantic version (for example "3.x") which will try to resolve
+	// to the best matching Catalog existing on the cluster.
 	RuntimeVersion string `property:"runtime-version" json:"runtimeVersion,omitempty"`
 	// A list of properties to be provided to the Integration runtime
 	Properties []string `property:"properties" json:"properties,omitempty"`
