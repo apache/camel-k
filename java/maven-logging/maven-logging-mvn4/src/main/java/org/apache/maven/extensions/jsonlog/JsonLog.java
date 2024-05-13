bab673b72e0d4fc8469772843f51104b13bf9e02
Guillaume Nodet <gnodet@gmail.com>
2024-05-13 09:41:21 +0200

    Align Maven 4 extension

--- java/maven-logging/maven-logging-mvn4/src/main/java/org/apache/maven/extensions/jsonlog/JsonLog.java
+++ java/maven-logging/maven-logging-mvn4/src/main/java/org/apache/maven/extensions/jsonlog/JsonLog.java
@@ -1,7 +1,10 @@
 package org.apache.maven.extensions.jsonlog;
 
+import org.apache.maven.cli.logging.Slf4jLoggerManager;
 import org.apache.maven.eventspy.AbstractEventSpy;
 import org.apache.maven.rtinfo.RuntimeInformation;
+import org.codehaus.plexus.MutablePlexusContainer;
+import org.codehaus.plexus.PlexusContainer;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
@@ -13,20 +16,20 @@ import java.lang.reflect.Field;
 @Singleton
 public class JsonLog extends AbstractEventSpy {
 
-    RuntimeInformation runtimeInformation;
-
     @Inject
-    JsonLog(RuntimeInformation runtimeInformation) throws Exception {
-        this.runtimeInformation = runtimeInformation;
-
+    JsonLog(RuntimeInformation runtimeInformation, PlexusContainer container) throws Exception {
         try {
             // Maven 4 extension code:
-            if (this.runtimeInformation.getMavenVersion().startsWith("4.")) {
+            if (runtimeInformation.getMavenVersion().startsWith("4.")) {
                 org.slf4j.spi.SLF4JServiceProvider provider = new ch.qos.logback.classic.spi.LogbackServiceProvider();
                 provider.initialize();
                 Field field = LoggerFactory.class.getDeclaredField("PROVIDER");
                 field.setAccessible(true);
                 field.set(null, provider);
+
+                if (container instanceof MutablePlexusContainer) {
+                    ((MutablePlexusContainer) container).setLoggerManager(new Slf4jLoggerManager());
+                }
 
                 LoggerFactory.getLogger("json-logging").warn("Json Logging initialized");
             }
