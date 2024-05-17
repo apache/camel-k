b6eb708b4d7eac3b03589c5c3ebd778875f10bcd
Vladislav Krejcirik <vkrejcirik@behaimits.com>
2024-05-17 13:37:53 -0500

    build configuration updated

--- pkg/client/camel/applyconfiguration/camel/v1/buildconfiguration.go
+++ pkg/client/camel/applyconfiguration/camel/v1/buildconfiguration.go
@@ -20,9 +20,8 @@ limitations under the License.
 package v1
 
 import (
-	corev1 "k8s.io/api/core/v1"
-
 	v1 "github.com/apache/camel-k/v2/pkg/apis/camel/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // BuildConfigurationApplyConfiguration represents an declarative configuration of the BuildConfiguration type for use
