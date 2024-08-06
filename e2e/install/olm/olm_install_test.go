00914c624888afa624863990cb0484c554f532d9
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2024-08-06 14:59:01 +0200

    chore(ci): simplify OLM testing

--- e2e/install/olm/olm_install_test.go
+++ e2e/install/olm/olm_install_test.go
@@ -26,91 +26,55 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 	"testing"
 
 	. "github.com/apache/camel-k/v2/e2e/support"
 	. "github.com/onsi/gomega"
-	"github.com/stretchr/testify/require"
 
 	corev1 "k8s.io/api/core/v1"
 
 	olm "github.com/operator-framework/api/pkg/operators/v1alpha1"
 
+	v1 "github.com/apache/camel-k/v2/pkg/apis/camel/v1"
 	"github.com/apache/camel-k/v2/pkg/util/defaults"
 	"github.com/apache/camel-k/v2/pkg/util/kubernetes"
-	"github.com/apache/camel-k/v2/pkg/util/openshift"
 )
 
 const installCatalogSourceName = "test-camel-k-source"
 
 func TestOLMInstallation(t *testing.T) {
 	WithNewTestNamespace(t, func(ctx context.Context, g *WithT, ns string) {
+		bundleImageName, ok := os.LookupEnv("BUNDLE_IMAGE_NAME")
+		g.Expect(ok).To(BeTrue())
+		containerRegistry, ok := os.LookupEnv("KAMEL_INSTALL_REGISTRY")
+		g.Expect(ok).To(BeTrue())
 		os.Setenv("CAMEL_K_TEST_MAKE_DIR", "../../../")
-		// Set the configuration required for OLM to work with local catalog
-		// Build Camel K local bundle
-		ExpectExecSucceed(t, g,
-			Make(t, "bundle-build"),
-		)
-		// Build Operator local index
-		ExpectExecSucceed(t, g,
-			Make(t, "bundle-index"),
-		)
-
-		// This workaround is required because of
-		// https://github.com/operator-framework/operator-lifecycle-manager/issues/903#issuecomment-1779366630
-		cmd := exec.Command("docker", "inspect", "--format={{.Id}}", "apache/camel-k-bundle-index:2.4.0-SNAPSHOT")
-		out, err := cmd.Output()
-		require.NoError(t, err)
-		newBundleIndexSha := strings.ReplaceAll(string(out), "\n", "")
-		g.Expect(newBundleIndexSha).To(Not(Equal("")))
-
-		newBundleIndex := fmt.Sprintf("apache/camel-k-bundle-index@%s", newBundleIndexSha)
-		g.Expect(CreateOrUpdateCatalogSource(t, ctx, ns, installCatalogSourceName, newBundleIndex)).To(Succeed())
-
-		ocp, err := openshift.IsOpenShift(TestClient(t))
-		require.NoError(t, err)
-
-		if ocp {
-			// Wait for pull secret to be created in namespace
-			// eg. test-camel-k-source-dockercfg-zlltn
-			secretPrefix := fmt.Sprintf("%s-dockercfg-", installCatalogSourceName)
-			g.Eventually(SecretByName(t, ctx, ns, secretPrefix), TestTimeoutLong).Should(Not(BeNil()))
-		}
-
-		g.Eventually(CatalogSourcePodRunning(t, ctx, ns, installCatalogSourceName), TestTimeoutMedium).Should(BeNil())
-		g.Eventually(CatalogSourcePhase(t, ctx, ns, installCatalogSourceName), TestTimeoutLong).Should(Equal("READY"))
-
-		// Install via OLM subscription (should use the latest default available channel)
-		ExpectExecSucceed(t, g,
-			exec.Command(
-				"kubectl",
-				"apply",
-				"-f",
-				"https://operatorhub.io/install/camel-k/camel-k.yaml",
+		// Install staged bundle (it must be available by building it before running the test)
+		// You can build it locally via `make bundle-push` action
+		ExpectExecSucceedWithTimeout(t, g,
+			Make(t,
+				"bundle-test",
+				fmt.Sprintf("BUNDLE_IMAGE_NAME=%s", bundleImageName),
+				fmt.Sprintf("NAMESPACE=%s", ns),
 			),
+			"180s",
 		)
-
 		// Find the only one Camel K CSV
 		noAdditionalConditions := func(csv olm.ClusterServiceVersion) bool {
 			return true
 		}
-		g.Eventually(ClusterServiceVersionPhase(t, ctx, noAdditionalConditions, ns), TestTimeoutMedium).Should(Equal(olm.CSVPhaseSucceeded))
-
-		// Refresh the test client to account for the newly installed CRDs
-		RefreshClient(t)
-
-		csvVersion := ClusterServiceVersion(t, ctx, noAdditionalConditions, ns)().Spec.Version
-		ipVersionPrefix := fmt.Sprintf("%d.%d", csvVersion.Version.Major, csvVersion.Version.Minor)
-		t.Logf("CSV Version installed: %s", csvVersion.Version.String())
-
+		g.Eventually(ClusterServiceVersionPhase(t, ctx, noAdditionalConditions, ns), TestTimeoutMedium).
+			Should(Equal(olm.CSVPhaseSucceeded))
 		// Check the operator pod is running
 		g.Eventually(OperatorPodPhase(t, ctx, ns), TestTimeoutMedium).Should(Equal(corev1.PodRunning))
 		g.Eventually(OperatorImage(t, ctx, ns), TestTimeoutShort).Should(Equal(defaults.OperatorImage()))
 
-		// Check the IntegrationPlatform has been reconciled
-		g.Eventually(PlatformVersion(t, ctx, ns)).Should(ContainSubstring(ipVersionPrefix))
+		// Check the IntegrationPlatform has been reconciled after setting the expected container registry
+		g.Expect(UpdatePlatform(t, ctx, ns, func(ip *v1.IntegrationPlatform) {
+			ip.Spec.Build.Registry.Address = containerRegistry
+		})).To(Succeed())
+		g.Eventually(PlatformPhase(t, ctx, ns), TestTimeoutMedium).Should(Equal(v1.IntegrationPlatformPhaseReady))
+		g.Eventually(PlatformVersion(t, ctx, ns), TestTimeoutMedium).Should(Equal(defaults.Version))
 
 		// Check if restricted security context has been applyed
 		operatorPod := OperatorPod(t, ctx, ns)()
@@ -118,5 +82,17 @@ func TestOLMInstallation(t *testing.T) {
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.Capabilities).To(Equal(kubernetes.DefaultOperatorSecurityContext().Capabilities))
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.SeccompProfile).To(Equal(kubernetes.DefaultOperatorSecurityContext().SeccompProfile))
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.AllowPrivilegeEscalation).To(Equal(kubernetes.DefaultOperatorSecurityContext().AllowPrivilegeEscalation))
+
+		// Test a simple integration is running
+		g.Expect(KamelRun(t, ctx, ns, "files/yaml.yaml").Execute()).To(Succeed())
+		g.Eventually(IntegrationPodPhase(t, ctx, ns, "yaml"), TestTimeoutMedium).Should(Equal(corev1.PodRunning))
+		g.Eventually(IntegrationConditionStatus(t, ctx, ns, "yaml", v1.IntegrationConditionReady), TestTimeoutShort).Should(Equal(corev1.ConditionTrue))
+		g.Eventually(IntegrationLogs(t, ctx, ns, "yaml"), TestTimeoutShort).Should(ContainSubstring("Magicstring!"))
+
+		// TODO Remove OLM CSV and test
+		// g.Eventually(OperatorPod(t, ctx, ns)).Should(BeNil())
+		// g.Eventually(Platform(t, ctx, ns)).Should(BeNil())
+		// g.Eventually(Integration(t, ctx, ns, "yaml"), TestTimeoutShort).ShouldNot(BeNil())
+		// g.Eventually(IntegrationConditionStatus(t, ctx, ns, "yaml", v1.IntegrationConditionReady), TestTimeoutShort).Should(Equal(corev1.ConditionTrue))
 	})
 }
