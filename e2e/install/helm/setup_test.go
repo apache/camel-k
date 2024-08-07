b6bb711227146a710626a94ad0e2a73b28922cf7
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2024-08-07 14:10:58 +0200

    chore(ci): Helm CRDs refactoring

--- e2e/install/helm/setup_test.go
+++ e2e/install/helm/setup_test.go
@@ -38,13 +38,12 @@ import (
 )
 
 func TestHelmInstallation(t *testing.T) {
-	containerRegistry, ok := os.LookupEnv("KAMEL_INSTALL_REGISTRY")
-	g.Expect(ok).To(BeTrue())
-
 	WithNewTestNamespace(t, func(ctx context.Context, g *WithT, ns string) {
+		containerRegistry, ok := os.LookupEnv("KAMEL_INSTALL_REGISTRY")
+		g.Expect(ok).To(BeTrue())
 		// Let's make sure no CRD is yet available in the cluster
-		// as we must make the Helm procedure to install them accordingly
-		g.Eventually(CRDs(t)).Should(BeNil())
+		// as we must make the procedure to install them accordingly
+		g.Eventually(CRDs(t)).Should(BeNil(), "No Camel K CRDs should be previously installed for this test")
 		operatorID := "helm-ck"
 		os.Setenv("CAMEL_K_TEST_MAKE_DIR", "../../../")
 		ExpectExecSucceed(t, g,
@@ -66,14 +65,14 @@ func TestHelmInstallation(t *testing.T) {
 		)
 
 		g.Eventually(OperatorPod(t, ctx, ns)).ShouldNot(BeNil())
-		// Check if restricted security context has been applyed
+		// Check if restricted security context has been applied
 		operatorPod := OperatorPod(t, ctx, ns)()
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.RunAsNonRoot).To(Equal(kubernetes.DefaultOperatorSecurityContext().RunAsNonRoot))
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.Capabilities).To(Equal(kubernetes.DefaultOperatorSecurityContext().Capabilities))
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.SeccompProfile).To(Equal(kubernetes.DefaultOperatorSecurityContext().SeccompProfile))
 		g.Expect(operatorPod.Spec.Containers[0].SecurityContext.AllowPrivilegeEscalation).To(Equal(kubernetes.DefaultOperatorSecurityContext().AllowPrivilegeEscalation))
 
-		//Test a simple route
+		// Test a simple route
 		t.Run("simple route", func(t *testing.T) {
 			name := RandomizedSuffixName("yaml")
 			g.Expect(KamelRunWithID(t, ctx, operatorID, ns, "files/yaml.yaml", "--name", name).Execute()).To(Succeed())
