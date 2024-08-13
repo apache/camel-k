9de3a46d83835717c6466d903c69cf6111aadf2f
Luis Sergio Carneiro <luis.carneiro@sensedia.com>
2024-08-13 20:29:53 -0300

    bugfix(#5758): changing incremental image build to not allow extra dependencies

--- pkg/apis/camel/v1/build_types_support.go
+++ pkg/apis/camel/v1/build_types_support.go
@@ -282,10 +282,14 @@ func (bl BuildList) HasMatchingBuild(build *Build) (bool, *Build) {
 	if len(required) == 0 {
 		return false, nil
 	}
+	requiredMap := make(map[string]int, len(required))
+	for i, item := range required {
+		requiredMap[item] = i
+	}
 	runtimeVersion := build.RuntimeVersion()
 
-	for i := range bl.Items {
-		b := bl.Items[i]
+buildLoop:
+	for _, b := range bl.Items {
 		if b.Name == build.Name || b.Status.IsFinished() {
 			continue
 		}
@@ -301,12 +305,18 @@ func (bl BuildList) HasMatchingBuild(build *Build) (bool, *Build) {
 			dependencyMap[item] = i
 		}
 
-		allMatching := true
+		// check if this build has any extra dependency. If it does, we can't use it
+		for _, item := range dependencies {
+			if _, ok := requiredMap[item]; !ok {
+				continue buildLoop
+			}
+		}
+
+		// now check how many common dependencies this build has
 		missing := 0
 		commonDependencies := 0
 		for _, item := range required {
 			if _, ok := dependencyMap[item]; !ok {
-				allMatching = false
 				missing++
 			} else {
 				commonDependencies++
@@ -314,6 +324,7 @@ func (bl BuildList) HasMatchingBuild(build *Build) (bool, *Build) {
 		}
 
 		if commonDependencies < len(required)/2 {
+			// few common dependencies
 			continue
 		}
 
@@ -323,20 +334,20 @@ func (bl BuildList) HasMatchingBuild(build *Build) (bool, *Build) {
 			return true, &b
 		case BuildPhaseInitialization, BuildPhaseScheduling:
 			// handle suitable scheduled build
-			if allMatching && len(required) == len(dependencies) {
+
+			// the build has at least half the dependencies, maybe all of them
+			if compareBuilds(&b, build) < 0 {
+				return true, &b
+			}
+			if missing == 0 {
 				// seems like both builds require exactly the same list of dependencies
 				// additionally check for the creation timestamp
 				if compareBuilds(&b, build) < 0 {
 					return true, &b
 				}
-			} else if !allMatching && commonDependencies > 0 {
-				// there are common dependencies. let's compare the total number of dependencies
-				// in each build. whichever build has less dependencies should run first
-				if len(dependencies) < len(required) ||
-					len(dependencies) == len(required) && compareBuilds(&b, build) < 0 {
-					return true, &b
-				}
-				continue
+			} else {
+				// some deps are missing, but this build has at least half the deps we need and no extra dependency.
+				return true, &b
 			}
 		}
 	}
