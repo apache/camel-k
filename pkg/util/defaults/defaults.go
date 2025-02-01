7e90c7bef1ae6add36265b70ef872a8ee9816f2e
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2025-02-01 09:52:21 +0100

    chore(dependencies): update runtime default to 3.15.2
    Ref #6062

--- pkg/util/defaults/defaults.go
+++ pkg/util/defaults/defaults.go
@@ -26,7 +26,7 @@ const (
 	Version = "2.6.0-SNAPSHOT"
 
 	// DefaultRuntimeVersion --
-	DefaultRuntimeVersion = "3.15.0"
+	DefaultRuntimeVersion = "3.15.2"
 
 	// baseImage --
 	baseImage = "eclipse-temurin:17-jdk"
