1f3ac46375874c50588a1fc69fa8f0e781467a26
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2025-02-08 11:07:55 +0100

    fix(release): push staging images

--- script/git_tag.sh
+++ script/git_tag.sh
@@ -28,14 +28,14 @@ target_tag=v$target_version
 target_staging=staging-$target_tag
 target_remote=$2
 
-git add *
-git commit -m "chore(release): preparing $target_version"
-git push
+git add helm docs || true
+git commit -m "chore(release): Helm chart for $target_version"
+git push -f
 
 git branch -D ${target_staging} || true
 git checkout -b ${target_staging}
-git add * || true
-git commit -a -m "Release ${target_version}" || true
+git add pkg/resources script || true
+git commit -m "chore(release): preparing for tag v$target_version" || true
 
 git tag --force ${target_tag} ${target_staging}
 git push --force ${target_remote} ${target_tag}
