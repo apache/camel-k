044667342439ec0521490a3814477a255972cc80
Keerthan <poojarykeerthan163@gmail.com>
2026-09-17 18:58:46 +0530

    pushed formatted fix

--- pkg/cmd/operator/operator.go
+++ pkg/cmd/operator/operator.go
@@ -202,10 +202,9 @@ func Run(healthPort, monitoringPort int32, leaderElection bool, leaderElectionID
 		selectors[&batchv1.CronJob{}] = selector
 	}
 
-	if platform.CertManagerInstalled, err= kubernetes.IsAPIResourceInstalled(bootstrapClient, "cert-manager.io/v1", "ClusterIssuer"); err!=nil{
+	if platform.CertManagerInstalled, err = kubernetes.IsAPIResourceInstalled(bootstrapClient, "cert-manager.io/v1", "ClusterIssuer"); err != nil {
 		log.Error(err, "failed to check for cert-manager presence, assuming not installed")
 	}
-
 
 	options := cache.Options{
 		ByObject: selectors,
